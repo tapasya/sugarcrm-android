4e5657b27cc758034d679b11cf20c40462181fe7
vasavin <vasavin>
2010-06-22 13:44:32 +0000

    All the constants that are used for the REST calls are put here.

---
+++ src/com/imaginea/android/sugarcrm/RestUtilConstants.java
@@ -0,0 +1,31 @@
+package com.imaginea.android.sugarcrm;
+
+public interface RestUtilConstants {
+
+	// request params
+	public static final String METHOD = "method";
+	public static final String INPUT_TYPE = "input_type";
+	public static final String RESPONSE_TYPE = "response_type";
+	public static final String REST_DATA = "rest_data";
+	public static final String APPLICATION = "application";
+	public static final String NAME_VALUE_LIST = "name_value_list";
+	
+	// input and response types for the request and response
+	public static final String JSON = "json";
+	
+	// methods
+	public static final String LOGIN = "login";
+	public static final String GET_AVAILABLE_MODULES = "get_available_modules";
+	
+	// params for the input JSON
+	public static final String USER_AUTH = "user_auth";
+	public static final String USER_NAME = "user_name";
+	public static final String PASSWORD = "password";
+	public static final String SESSION = "session";
+	
+	// params for the output JSON
+	public static final String ID = "id";
+	public static final String MODULES = "modules";
+	
+	
+}
