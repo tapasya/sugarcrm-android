e932b03c1edd2da87ac608f17cd54a949c897499
vasavin <vasavin>
2010-06-22 13:43:44 +0000

    added getAvailableModules method that retrieves the modules using the sessonId obtained after successful login using the login method.

--- src/com/imaginea/android/sugarcrm/LoginActivity.java
+++ src/com/imaginea/android/sugarcrm/LoginActivity.java
@@ -1,12 +1,10 @@
 package com.imaginea.android.sugarcrm;
 
-import java.io.InputStream;
-import java.io.StringWriter;
 import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import org.apache.http.HttpResponse;
@@ -16,14 +14,22 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.imaginea.android.sugarcrm.util.Util;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 
+import static com.imaginea.android.sugarcrm.RestUtilConstants.*;
+
 public class LoginActivity extends Activity{
+	
+	public final static String LOG_TAG = "LoginActivity";
+	
 	/** Called when the activity is first created. */
 	@Override
     public void onCreate(Bundle savedInstanceState) {
@@ -33,8 +39,11 @@ public class LoginActivity extends Activity{
         String password = "will";
         String md5Password;
 		try {
-			md5Password = MD5(password);
-			loginToSugarCRM(url, username, md5Password);
+			md5Password = Util.MD5(password);
+			String sessionId = loginToSugarCRM(url, username, md5Password);
+			Log.i(LOG_TAG, sessionId);
+			List<String> modules = getAvailableModules(url, sessionId);
+			Log.i(LOG_TAG, modules.toString());
 		} catch (NoSuchAlgorithmException e) {
 			e.printStackTrace();
 		} catch (UnsupportedEncodingException e) {
@@ -47,84 +56,77 @@ public class LoginActivity extends Activity{
     }
 
 
-	private void loginToSugarCRM(String url, String username, String password) throws Exception {
-		//http://192.168.1.83/sugarcrm/service/v2/rest.php?method=login&input_type=json&response_type=json&rest_data={"user_auth":{"password":"18218139eec55d83cf82679934e5cd75","username":"will"}}
+	private List<String> getAvailableModules(String url, String sessionId) throws Exception {
+		
+		JSONObject sessId = new JSONObject();
+		sessId.put(SESSION, sessionId);
+		
+		HttpClient httpClient = new DefaultHttpClient();
+		HttpPost req = new HttpPost(url);
+		
+		// Add your data  
+		List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+        nameValuePairs.add(new BasicNameValuePair(METHOD, GET_AVAILABLE_MODULES));
+        nameValuePairs.add(new BasicNameValuePair(INPUT_TYPE, JSON ));
+        nameValuePairs.add(new BasicNameValuePair(RESPONSE_TYPE, JSON ));
+        nameValuePairs.add(new BasicNameValuePair(REST_DATA, sessId.toString()));
+        
+        req.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+        
+        // Send POST request
+        HttpResponse resLogin = httpClient.execute(req);
+
+        if (resLogin.getEntity() == null) {
+			Log.i(LOG_TAG, "FAILED TO CONNECT!");
+			return Collections.EMPTY_LIST;
+		}
+        
+        final String response = EntityUtils.toString(resLogin.getEntity());
+        Log.i(LOG_TAG, response);
+        JSONObject responseObj = new JSONObject(response);
+
+        return new ArrayList<String>(Arrays.asList(responseObj.get(MODULES).toString().split(",")));
+	}
+
+
+	private String loginToSugarCRM(String url, String username, String password) throws Exception {
 		
 		JSONObject credentials = new JSONObject();
-		credentials.put("user_name", username);
-		credentials.put("password", password);
+		credentials.put(USER_NAME, username);
+		credentials.put(PASSWORD, password);
 		
 		JSONArray jsonArray = new JSONArray();
-		jsonArray.add(credentials);
+		jsonArray.put(credentials);
 		
 		JSONObject userAuth = new JSONObject();
-		userAuth.put("user_auth", credentials);
-		
-		Log.i("msg", userAuth+"");
-		
-		StringWriter out = new StringWriter();
-		userAuth.writeJSONString(out);
-		String jsonText = out.toString();
-		//String reqUrl = url + "?method=login&input_type=json&response_type=json&rest_data=" + URLEncoder.encode(jsonText);
+		userAuth.put(USER_AUTH, credentials);		
 		
 		HttpClient httpClient = new DefaultHttpClient();
 		HttpPost reqLogin = new HttpPost(url);
 		// Add your data  
-		List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
-        nameValuePairs.add(new BasicNameValuePair("method", "login"));
-        nameValuePairs.add(new BasicNameValuePair("input_type", "json" ));
-        nameValuePairs.add(new BasicNameValuePair("response_type", "json" ));
-        nameValuePairs.add(new BasicNameValuePair("rest_data", jsonText ));
-        nameValuePairs.add(new BasicNameValuePair("application", "" ));
-        nameValuePairs.add(new BasicNameValuePair("name_value_list", "" ));
+		List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+        nameValuePairs.add(new BasicNameValuePair(METHOD, LOGIN));
+        nameValuePairs.add(new BasicNameValuePair(INPUT_TYPE, JSON ));
+        nameValuePairs.add(new BasicNameValuePair(RESPONSE_TYPE, JSON ));
+        nameValuePairs.add(new BasicNameValuePair(REST_DATA, userAuth.toString() ));
+        nameValuePairs.add(new BasicNameValuePair(APPLICATION, "" ));
+        nameValuePairs.add(new BasicNameValuePair(NAME_VALUE_LIST, "" ));
           
         reqLogin.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-                
-
+        
         // Send POST request
         HttpResponse resLogin = httpClient.execute(reqLogin);
 
         if (resLogin.getEntity() == null) {
 			System.out.println("FAILED TO CONNECT.");
-			Log.i("msg", "FAILED TO CONNECT!");
-			return;
+			Log.i(LOG_TAG, "FAILED TO CONNECT!");
+			return "";
 		}
-        int read = 0;
-        byte buffer[] = new byte[8192]; 
-        InputStream responseBodyStream = resLogin.getEntity().getContent();
-        StringBuffer responseBody = new StringBuffer();
-        while ((read = responseBodyStream.read(buffer)) != -1)
-        {
-            responseBody.append(new String(buffer, 0, read));
-        }
         
-        String res = responseBody.toString();
-        Log.i("msg", res);
+        final String response = EntityUtils.toString(resLogin.getEntity());
+        JSONObject responseObj = new JSONObject(response);
+        return responseObj.get(ID).toString();
 		
 	}
-	
-	public static String MD5(String text) throws NoSuchAlgorithmException, UnsupportedEncodingException {
-        MessageDigest md = MessageDigest.getInstance("MD5");
-        byte[] md5hash = new byte[32];
-        md.update(text.getBytes("iso-8859-1"), 0, text.length());
-        md5hash = md.digest();
-        return convertToHex(md5hash);
-    }
-	
-	private static String convertToHex(byte[] data) {
-        StringBuffer buf = new StringBuffer();
-        for (int i = 0; i < data.length; i++) {
-            int halfbyte = (data[i] >>> 4) & 0x0F;
-            int two_halfs = 0;
-            do {
-                if ((0 <= halfbyte) && (halfbyte <= 9))
-                    buf.append((char) ('0' + halfbyte));
-                else
-                    buf.append((char) ('a' + (halfbyte - 10)));
-                halfbyte = data[i] & 0x0F;
-            } while(two_halfs++ < 1);
-        }
-        return buf.toString();
-    }
 
 }
