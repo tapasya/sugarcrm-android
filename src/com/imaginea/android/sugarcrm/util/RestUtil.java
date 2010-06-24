2f47067d177d13b6e651484c928d65b21782ffc7
vasavin <vasavin>
2010-06-24 13:04:45 +0000

    updated the getEntryList method to return the Map<String, Map<String, Object>> , i.e. name-value pairs.

--- src/com/imaginea/android/sugarcrm/util/RestUtil.java
+++ src/com/imaginea/android/sugarcrm/util/RestUtil.java
@@ -1,6 +1,7 @@
 package com.imaginea.android.sugarcrm.util;
 
 import static com.imaginea.android.sugarcrm.RestUtilConstants.APPLICATION;
+import static com.imaginea.android.sugarcrm.RestUtilConstants.FIELDS;
 import static com.imaginea.android.sugarcrm.RestUtilConstants.GET_AVAILABLE_MODULES;
 import static com.imaginea.android.sugarcrm.RestUtilConstants.GET_ENTRY_LIST;
 import static com.imaginea.android.sugarcrm.RestUtilConstants.GET_MODULE_FIELDS;
@@ -37,12 +38,18 @@ import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.simple.parser.ContainerFactory;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -52,42 +59,59 @@ public class RestUtil {
 
     private static HttpClient httpClient = new DefaultHttpClient();
 
-    public static List<String> getAvailableModules(String url, String sessionId)
-                                    throws JSONException, ClientProtocolException, IOException {
-        JSONObject sessId = new JSONObject();
-        sessId.put(SESSION, sessionId);
+    /**
+     * Retrieve a list of beans. This is the primary method for getting list of SugarBeans from
+     * Sugar using the SOAP API.
+     * 
+     * @param String
+     *            $session -- Session ID returned by a previous call to login.
+     * @param String
+     *            $module_name -- The name of the module to return records from. This name should be
+     *            the name the module was developed under (changing a tab name is studio does not
+     *            affect the name that should be passed into this method)..
+     * @param String
+     *            $query -- SQL where clause without the word 'where'
+     * @param String
+     *            $order_by -- SQL order by clause without the phrase 'order by'
+     * @param integer
+     *            $offset -- The record offset to start from.
+     * @param Array
+     *            $select_fields -- A list of the fields to be included in the results. This
+     *            optional parameter allows for only needed fields to be retrieved.
+     * @param Array
+     *            $link_name_to_fields_array -- A list of link_names and for each link_name, what
+     *            fields value to be returned. For ex.'link_name_to_fields_array' =>
+     *            array(array('name' => 'email_addresses', 'value' => array('id', 'email_address',
+     *            'opt_out', 'primary_address')))
+     * @param integer
+     *            $max_results -- The maximum number of records to return. The default is the sugar
+     *            configuration value for 'list_max_entries_per_page'
+     * @param integer
+     *            $deleted -- false if deleted records should not be include, true if deleted
+     *            records should be included.
+     * @return Array 'result_count' -- integer - The number of records returned 'next_offset' --
+     *         integer - The start of the next page (This will always be the previous offset plus
+     *         the number of rows returned. It does not indicate if there is additional data unless
+     *         you calculate that the next_offset happens to be closer than it should be.
+     *         'entry_list' -- Array - The records that were retrieved 'relationship_list' -- Array
+     *         - The records link field data. The example is if asked about accounts email address
+     *         then return data would look like Array ( [0] => Array ( [name] => email_addresses
+     *         [records] => Array ( [0] => Array ( [0] => Array ( [name] => id [value] =>
+     *         3fb16797-8d90-0a94-ac12-490b63a6be67 ) [1] => Array ( [name] => email_address [value]
+     *         => hr.kid.qa@example.com ) [2] => Array ( [name] => opt_out [value] => 0 ) [3] =>
+     *         Array ( [name] => primary_address [value] => 1 ) ) [1] => Array ( [0] => Array (
+     *         [name] => id [value] => 403f8da1-214b-6a88-9cef-490b63d43566 ) [1] => Array ( [name]
+     *         => email_address [value] => kid.hr@example.name ) [2] => Array ( [name] => opt_out
+     *         [value] => 0 ) [3] => Array ( [name] => primary_address [value] => 0 ) ) ) ) )
+     * @exception 'SoapFault' -- The SOAP error, if any
+     */
+    public static Map<String, Map<String, Object>> getEntryList(String url, String sessionId,
+                                    String moduleName, String query, String orderBy, int offset,
+                                    String[] selectFields, String[] linkNameToFieldsArray,
+                                    int maxResults, int deleted) throws JSONException,
+                                    ClientProtocolException, IOException {
 
-        HttpPost req = new HttpPost(url);
-        // Add your data
-        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
-        nameValuePairs.add(new BasicNameValuePair(METHOD, GET_AVAILABLE_MODULES));
-        nameValuePairs.add(new BasicNameValuePair(INPUT_TYPE, JSON));
-        nameValuePairs.add(new BasicNameValuePair(RESPONSE_TYPE, JSON));
-        nameValuePairs.add(new BasicNameValuePair(REST_DATA, sessId.toString()));
-        req.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-
-        // Send POST request
-        HttpResponse res = httpClient.execute(req);
-
-        if (res.getEntity() == null) {
-            Log.i(LOG_TAG, "FAILED TO CONNECT!");
-            return Collections.emptyList();
-        }
-
-        final String response = EntityUtils.toString(res.getEntity());
-        Log.i(LOG_TAG, "available modules : " + response);
-        JSONObject responseObj = new JSONObject(response);
-
-        return new ArrayList<String>(Arrays.asList(responseObj.get(MODULES).toString().split(",")));
-    }
-
-    public static void getEntryList(String url, String sessionId, String moduleName, String query,
-                                    String orderBy, int offset, String[] selectFields,
-                                    String[] linkNameToFieldsArray, int maxResults, int deleted)
-                                    throws JSONException, ClientProtocolException, IOException {
-        JSONArray arr = new JSONArray();
-        arr.put("name");
-        arr.put("date_modified");
+        JSONArray fieldsArr = new JSONArray(Arrays.asList(selectFields));
 
         Map<String, Object> data = new LinkedHashMap<String, Object>();
         data.put(SESSION, sessionId);
@@ -95,7 +119,9 @@ public class RestUtil {
         data.put(QUERY, "");
         data.put(ORDER_BY, "");
         data.put(OFFSET, "");
-        data.put(SELECT_FIELDS, arr);
+        data.put(SELECT_FIELDS, fieldsArr);
+        data.put("link_name_to_fields_array", "");
+        data.put("max_results", "5");
 
         String restData = org.json.simple.JSONValue.toJSONString(data);
         Log.i(LOG_TAG, "restData : " + restData);
@@ -114,54 +140,49 @@ public class RestUtil {
         HttpResponse res = httpClient.execute(req);
         if (res.getEntity() == null) {
             Log.i(LOG_TAG, "FAILED TO CONNECT!");
-            // return Collections.EMPTY_LIST;
-            return;
+            return Collections.emptyMap();
         }
         final String response = EntityUtils.toString(res.getEntity());
-        Log.i(LOG_TAG, "entryList : " + response);
+        JSONObject responseObj = new JSONObject(response);
+        JSONArray entryListJson = responseObj.getJSONArray("entry_list");
+        Map<String, Map<String, Object>> map = new HashMap<String, Map<String, Object>>();
+        for (int i = 0; i < entryListJson.length(); i++) {
+            String id = ((JSONObject) entryListJson.get(i)).get("id").toString();
+            String nameValueList = ((JSONObject) entryListJson.get(i)).get("name_value_list").toString();
 
-        // JSONObject responseObj = new JSONObject(response);
-        // return new
-        // ArrayList<String>(Arrays.asList(responseObj.get(ENTRY_LIST).toString().split(",")));
-    }
-
-    public static void getModuleFields(String url, String sessionId, String moduleName,
-                                    String[] fields) throws JSONException, ClientProtocolException,
-                                    IOException {
-        Map<String, String> data = new LinkedHashMap<String, String>();
-        data.put(SESSION, sessionId);
-        data.put(MODULE_NAME, moduleName);
-
-        String restData = org.json.simple.JSONValue.toJSONString(data);
-
-        HttpClient httpClient = new DefaultHttpClient();
-        HttpPost req = new HttpPost(url);
-        // Add your data
-        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
-        nameValuePairs.add(new BasicNameValuePair(METHOD, GET_MODULE_FIELDS));
-        nameValuePairs.add(new BasicNameValuePair(INPUT_TYPE, JSON));
-        nameValuePairs.add(new BasicNameValuePair(RESPONSE_TYPE, JSON));
-        nameValuePairs.add(new BasicNameValuePair(REST_DATA, restData.toString()));
-        req.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-        Log.i(LOG_TAG, EntityUtils.toString(req.getEntity()));
-
-        // Send POST request
-        HttpResponse res = httpClient.execute(req);
-
-        if (res.getEntity() == null) {
-            Log.i(LOG_TAG, "FAILED TO CONNECT!");
-            // return (ArrayList<String>) Collections.EMPTY_LIST;
-            return;
+            JSONObject nameVal = new JSONObject(nameValueList);
+            Iterator iter = nameVal.keys();
+            Map<String, Object> fields = new HashMap<String, Object>();
+            while (iter.hasNext()) {
+                String key = (String) iter.next();
+                String val = ((JSONObject) (nameVal.get(key))).get("value").toString();
+                fields.put(key, val);
+                Log.i(LOG_TAG, key + " : " + val);
+            }
+            map.put(id, fields);
         }
-
-        final String response = EntityUtils.toString(res.getEntity());
-        Log.i(LOG_TAG, "moduleFields : " + response);
-
-        // JSONObject responseObj = new JSONObject(response);
-        // return new
-        // ArrayList<String>(Arrays.asList(responseObj.get(MODULE_FIELDS).toString().split(",")));
+        Log.i(LOG_TAG, map.toString());
+        return map;
     }
 
+    /**
+     * Log the user into the application
+     * 
+     * @param UserAuth
+     *            array $user_auth -- Set user_name and password (password needs to be in the right
+     *            encoding for the type of authentication the user is setup for. For Base sugar
+     *            validation, password is the MD5 sum of the plain text password.
+     * @param String
+     *            $application -- The name of the application you are logging in from. (Currently
+     *            unused).
+     * @param array
+     *            $name_value_list -- Array of name value pair of extra parameters. As of today only
+     *            'language' and 'notifyonsave' is supported
+     * @return Array - id - String id is the session_id of the session that was created. -
+     *         module_name - String - module name of user - name_value_list - Array - The name value
+     *         pair of user_id, user_name, user_language, user_currency_id, user_currency_name
+     * @exception 'SoapFault' -- The SOAP error, if any
+     */
     public static String loginToSugarCRM(String url, String username, String password)
                                     throws JSONException, ClientProtocolException, IOException {
         JSONObject credentials = new JSONObject();
@@ -201,4 +222,95 @@ public class RestUtil {
         return responseObj.get(ID).toString();
     }
 
+    /**
+     * Retrieve the list of available modules on the system available to the currently logged in
+     * user.
+     * 
+     * @param String
+     *            $session -- Session ID returned by a previous call to login.
+     * @return Array 'modules' -- Array - An array of module names
+     * @exception 'SoapFault' -- The SOAP error, if any
+     */
+    public static List<String> getAvailableModules(String url, String sessionId)
+                                    throws JSONException, ClientProtocolException, IOException {
+        JSONObject sessId = new JSONObject();
+        sessId.put(SESSION, sessionId);
+
+        HttpPost req = new HttpPost(url);
+        // Add your data
+        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+        nameValuePairs.add(new BasicNameValuePair(METHOD, GET_AVAILABLE_MODULES));
+        nameValuePairs.add(new BasicNameValuePair(INPUT_TYPE, JSON));
+        nameValuePairs.add(new BasicNameValuePair(RESPONSE_TYPE, JSON));
+        nameValuePairs.add(new BasicNameValuePair(REST_DATA, sessId.toString()));
+        req.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+        // Send POST request
+        HttpResponse res = httpClient.execute(req);
+
+        if (res.getEntity() == null) {
+            Log.i(LOG_TAG, "FAILED TO CONNECT!");
+            return Collections.emptyList();
+        }
+
+        final String response = EntityUtils.toString(res.getEntity());
+        Log.i(LOG_TAG, "available modules : " + response);
+        JSONObject responseObj = new JSONObject(response);
+
+        return new ArrayList<String>(Arrays.asList(responseObj.get(MODULES).toString().split(",")));
+    }
+
+    // TODO parse the response to get module fields and change the return type
+    /**
+     * Retrieve vardef information on the fields of the specified bean.
+     * 
+     * @param String
+     *            $session -- Session ID returned by a previous call to login.
+     * @param String
+     *            $module_name -- The name of the module to return records from. This name should be
+     *            the name the module was developed under (changing a tab name is studio does not
+     *            affect the name that should be passed into this method)..
+     * @param Array
+     *            $fields -- Optional, if passed then retrieve vardef information on these fields
+     *            only.
+     * @return Array 'module_fields' -- Array - The vardef information on the selected fields.
+     *         'link_fields' -- Array - The vardef information on the link fields
+     * @exception 'SoapFault' -- The SOAP error, if any
+     */
+    public static void getModuleFields(String url, String sessionId, String moduleName,
+                                    String[] fields) throws JSONException, ClientProtocolException,
+                                    IOException {
+        Map<String, Object> data = new LinkedHashMap<String, Object>();
+        data.put(SESSION, sessionId);
+        data.put(MODULE_NAME, moduleName);
+
+        JSONArray arr = new JSONArray(Arrays.asList(fields));
+        data.put(FIELDS, arr);
+
+        String restData = org.json.simple.JSONValue.toJSONString(data);
+
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpPost req = new HttpPost(url);
+        // Add your data
+        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+        nameValuePairs.add(new BasicNameValuePair(METHOD, GET_MODULE_FIELDS));
+        nameValuePairs.add(new BasicNameValuePair(INPUT_TYPE, JSON));
+        nameValuePairs.add(new BasicNameValuePair(RESPONSE_TYPE, JSON));
+        nameValuePairs.add(new BasicNameValuePair(REST_DATA, restData.toString()));
+        req.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+        Log.i(LOG_TAG, EntityUtils.toString(req.getEntity()));
+
+        // Send POST request
+        HttpResponse res = httpClient.execute(req);
+
+        if (res.getEntity() == null) {
+            Log.i(LOG_TAG, "FAILED TO CONNECT!");
+            // return (ArrayList<String>) Collections.EMPTY_LIST;
+            return;
+        }
+
+        final String response = EntityUtils.toString(res.getEntity());
+        Log.i(LOG_TAG, "moduleFields : " + response);
+        // JSONObject jsonResponse = new JSONObject(response);
+    }
 }
