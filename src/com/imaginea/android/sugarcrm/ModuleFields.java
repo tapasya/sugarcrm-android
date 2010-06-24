5f2da61c35d96765de883cef2be21d880844287c
chanderp <chanderp>
2010-06-24 13:23:45 +0000

    initial version

---
+++ src/com/imaginea/android/sugarcrm/ModuleFields.java
@@ -0,0 +1,311 @@
+package com.imaginea.android.sugarcrm;
+
+/**
+ * Module fields for Accounts, Contacts, Opportunities, Leads, Campaigns, Meetings, Cases.
+ * 
+ * Velocity is used to generate this file, please do not edit manually
+ * 
+ * @author chander
+ *
+ */
+public interface ModuleFields {
+
+    public static final String DATE_ENTERED = "date_entered";
+
+    public static final String SIC_CODE = "sic_code";
+
+    public static final String BILLING_ADDRESS_STREET = "billing_address_street";
+
+    public static final String SHIPPING_ADDRESS_POSTALCODE = "shipping_address_postalcode";
+
+    public static final String ASSIGNED_USER_ID = "assigned_user_id";
+
+    public static final String BILLING_ADDRESS_CITY = "billing_address_city";
+
+    public static final String EMAIL_OPT_OUT = "email_opt_out";
+
+    public static final String CREATED_BY_NAME = "created_by_name";
+
+    public static final String ID = "id";
+
+    public static final String PHONE_FAX = "phone_fax";
+
+    public static final String INVALID_EMAIL = "invalid_email";
+
+    public static final String PHONE_ALTERNATE = "phone_alternate";
+
+    public static final String BILLING_ADDRESS_STATE = "billing_address_state";
+
+    public static final String DESCRIPTION = "description";
+
+    public static final String NAME = "name";
+
+    public static final String SHIPPING_ADDRESS_COUNTRY = "shipping_address_country";
+
+    public static final String SHIPPING_ADDRESS_STREET_2 = "shipping_address_street_2";
+
+    public static final String BILLING_ADDRESS_STREET_3 = "billing_address_street_3";
+
+    public static final String SHIPPING_ADDRESS_STREET_3 = "shipping_address_street_3";
+
+    public static final String BILLING_ADDRESS_STREET_4 = "billing_address_street_4";
+
+    public static final String BILLING_ADDRESS_POSTALCODE = "billing_address_postalcode";
+
+    public static final String BILLING_ADDRESS_STREET_2 = "billing_address_street_2";
+
+    public static final String INDUSTRY = "industry";
+
+    public static final String SHIPPING_ADDRESS_STREET_4 = "shipping_address_street_4";
+
+    public static final String PARENT_ID = "parent_id";
+
+    public static final String ACCOUNT_TYPE = "account_type";
+
+    public static final String CREATED_BY = "created_by";
+
+    public static final String SHIPPING_ADDRESS_STREET = "shipping_address_street";
+
+    public static final String WEBSITE = "website";
+
+    public static final String DATE_MODIFIED = "date_modified";
+
+    public static final String CAMPAIGN_NAME = "campaign_name";
+
+    public static final String ANNUAL_REVENUE = "annual_revenue";
+
+    public static final String MODIFIED_BY_NAME = "modified_by_name";
+
+    public static final String MODIFIED_USER_ID = "modified_user_id";
+
+    public static final String EMAIL1 = "email1";
+
+    public static final String DELETED = "deleted";
+
+    public static final String CAMPAIGN_ID = "campaign_id";
+
+    public static final String TICKER_SYMBOL = "ticker_symbol";
+
+    public static final String SHIPPING_ADDRESS_CITY = "shipping_address_city";
+
+    public static final String OWNERSHIP = "ownership";
+
+    public static final String PHONE_OFFICE = "phone_office";
+
+    public static final String PARENT_NAME = "parent_name";
+
+    public static final String SHIPPING_ADDRESS_STATE = "shipping_address_state";
+
+    public static final String RATING = "rating";
+
+    public static final String BILLING_ADDRESS_COUNTRY = "billing_address_country";
+
+    public static final String ASSIGNED_USER_NAME = "assigned_user_name";
+
+    public static final String EMPLOYEES = "employees";
+
+    public static final String OPPORTUNITY_ROLE_ID = "opportunity_role_id";
+
+    public static final String PRIMARY_ADDRESS_CITY = "primary_address_city";
+
+    public static final String SYNC_CONTACT = "sync_contact";
+
+    public static final String PRIMARY_ADDRESS_POSTALCODE = "primary_address_postalcode";
+
+    public static final String FIRST_NAME = "first_name";
+
+    public static final String ASSISTANT = "assistant";
+
+    public static final String ACCEPT_STATUS_ID = "accept_status_id";
+
+    public static final String ASSISTANT_PHONE = "assistant_phone";
+
+    public static final String ACCOUNT_ID = "account_id";
+
+    public static final String ALT_ADDRESS_STATE = "alt_address_state";
+
+    public static final String REPORT_TO_NAME = "report_to_name";
+
+    public static final String ACCOUNT_NAME = "account_name";
+
+    public static final String PHONE_HOME = "phone_home";
+
+    public static final String ALT_ADDRESS_STREET = "alt_address_street";
+
+    public static final String LEAD_SOURCE = "lead_source";
+
+    public static final String LAST_NAME = "last_name";
+
+    public static final String DO_NOT_CALL = "do_not_call";
+
+    public static final String ALT_ADDRESS_COUNTRY = "alt_address_country";
+
+    public static final String PRIMARY_ADDRESS_STREET_2 = "primary_address_street_2";
+
+    public static final String PRIMARY_ADDRESS_STREET_3 = "primary_address_street_3";
+
+    public static final String ALT_ADDRESS_CITY = "alt_address_city";
+
+    public static final String DEPARTMENT = "department";
+
+    public static final String REPORTS_TO_ID = "reports_to_id";
+
+    public static final String PRIMARY_ADDRESS_COUNTRY = "primary_address_country";
+
+    public static final String ACCEPT_STATUS_NAME = "accept_status_name";
+
+    public static final String ALT_ADDRESS_POSTALCODE = "alt_address_postalcode";
+
+    public static final String TITLE = "title";
+
+    public static final String BIRTHDATE = "birthdate";
+
+    public static final String OPPORTUNITY_ROLE = "opportunity_role";
+
+    public static final String ALT_ADDRESS_STREET_3 = "alt_address_street_3";
+
+    public static final String PRIMARY_ADDRESS_STATE = "primary_address_state";
+
+    public static final String C_ACCEPT_STATUS_FIELDS = "c_accept_status_fields";
+
+    public static final String PHONE_WORK = "phone_work";
+
+    public static final String OPPORTUNITY_ROLE_FIELDS = "opportunity_role_fields";
+
+    public static final String PRIMARY_ADDRESS_STREET = "primary_address_street";
+
+    public static final String EMAIL2 = "email2";
+
+    public static final String ALT_ADDRESS_STREET_2 = "alt_address_street_2";
+
+    public static final String M_ACCEPT_STATUS_FIELDS = "m_accept_status_fields";
+
+    public static final String PHONE_OTHER = "phone_other";
+
+    public static final String EMAIL_AND_NAME1 = "email_and_name1";
+
+    public static final String SALUTATION = "salutation";
+
+    public static final String PHONE_MOBILE = "phone_mobile";
+
+    public static final String FULL_NAME = "full_name";
+
+    public static final String AMOUNT = "amount";
+
+    public static final String AMOUNT_USDOLLAR = "amount_usdollar";
+
+    public static final String DATE_CLOSED = "date_closed";
+
+    public static final String CURRENCY_SYMBOL = "currency_symbol";
+
+    public static final String PROBABILITY = "probability";
+
+    public static final String CURRENCY_ID = "currency_id";
+
+    public static final String CURRENCY_NAME = "currency_name";
+
+    public static final String OPPORTUNITY_TYPE = "opportunity_type";
+
+    public static final String SALES_STAGE = "sales_stage";
+
+    public static final String NEXT_STEP = "next_step";
+
+    public static final String OPPORTUNITY_NAME = "opportunity_name";
+
+    public static final String STATUS_DESCRIPTION = "status_description";
+
+    public static final String WEBTOLEAD_EMAIL1 = "webtolead_email1";
+
+    public static final String CONTACT_ID = "contact_id";
+
+    public static final String WEBTOLEAD_EMAIL2 = "webtolead_email2";
+
+    public static final String LEAD_SOURCE_DESCRIPTION = "lead_source_description";
+
+    public static final String OPPORTUNITY_ID = "opportunity_id";
+
+    public static final String STATUS = "status";
+
+    public static final String REFERED_BY = "refered_by";
+
+    public static final String ACCOUNT_DESCRIPTION = "account_description";
+
+    public static final String OPPORTUNITY_AMOUNT = "opportunity_amount";
+
+    public static final String CONVERTED = "converted";
+
+    public static final String PORTAL_NAME = "portal_name";
+
+    public static final String PORTAL_APP = "portal_app";
+
+    public static final String WEBTOLEAD_EMAIL_OPT_OUT = "webtolead_email_opt_out";
+
+    public static final String WEBTOLEAD_INVALID_EMAIL = "webtolead_invalid_email";
+
+    public static final String BUDGET = "budget";
+
+    public static final String FREQUENCY = "frequency";
+
+    public static final String EXPECTED_REVENUE = "expected_revenue";
+
+    public static final String END_DATE = "end_date";
+
+    public static final String IMPRESSIONS = "impressions";
+
+    public static final String OBJECTIVE = "objective";
+
+    public static final String CAMPAIGN_TYPE = "campaign_type";
+
+    public static final String EXPECTED_COST = "expected_cost";
+
+    public static final String TRACKER_TEXT = "tracker_text";
+
+    public static final String TRACKER_COUNT = "tracker_count";
+
+    public static final String CONTENT = "content";
+
+    public static final String ACTUAL_COST = "actual_cost";
+
+    public static final String REFER_URL = "refer_url";
+
+    public static final String TRACKER_KEY = "tracker_key";
+
+    public static final String START_DATE = "start_date";
+
+    public static final String LOCATION = "location";
+
+    public static final String DIRECTION = "direction";
+
+    public static final String DATE_START = "date_start";
+
+    public static final String PARENT_TYPE = "parent_type";
+
+    public static final String CONTACT_NAME = "contact_name";
+
+    public static final String DURATION_HOURS = "duration_hours";
+
+    public static final String OUTLOOK_ID = "outlook_id";
+
+    public static final String REMINDER_TIME = "reminder_time";
+
+    public static final String ACCEPT_STATUS = "accept_status";
+
+    public static final String DURATION_MINUTES = "duration_minutes";
+
+    public static final String DATE_END = "date_end";
+
+    public static final String REMINDER_CHECKED = "reminder_checked";
+
+    public static final String ACCOUNT_NAME1 = "account_name1";
+
+    public static final String CASE_NUMBER = "case_number";
+
+    public static final String WORK_LOG = "work_log";
+
+    public static final String RESOLUTION = "resolution";
+
+    public static final String TYPE = "type";
+
+    public static final String PRIORITY = "priority";
+
+}
