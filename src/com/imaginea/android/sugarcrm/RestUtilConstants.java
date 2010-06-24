793ffff1b3a46395f4a71b98ec2686a61d7669ba
vasavin <vasavin>
2010-06-24 05:48:43 +0000

    *** empty log message ***

--- src/com/imaginea/android/sugarcrm/RestUtilConstants.java
+++ src/com/imaginea/android/sugarcrm/RestUtilConstants.java
@@ -2,47 +2,49 @@ package com.imaginea.android.sugarcrm;
 
 public interface RestUtilConstants {
 
-	// request params
-	public static final String METHOD = "method";
+    // request params
+    public static final String METHOD = "method";
 
-	public static final String INPUT_TYPE = "input_type";
+    public static final String INPUT_TYPE = "input_type";
 
-	public static final String RESPONSE_TYPE = "response_type";
+    public static final String RESPONSE_TYPE = "response_type";
 
-	public static final String REST_DATA = "rest_data";
+    public static final String REST_DATA = "rest_data";
 
-	public static final String APPLICATION = "application";
+    public static final String APPLICATION = "application";
 
-	public static final String NAME_VALUE_LIST = "name_value_list";
+    public static final String NAME_VALUE_LIST = "name_value_list";
 
-	// input and response types for the request and response
-	public static final String JSON = "json";
+    // input and response types for the request and response
+    public static final String JSON = "json";
 
-	// methods
-	public static final String LOGIN = "login";
+    // methods
+    public static final String LOGIN = "login";
 
-	public static final String GET_AVAILABLE_MODULES = "get_available_modules";
-	
-	public static final String GET_ENTRY_LIST = "get_entry_list";
-	
-	public static final String GET_MODULE_FIELDS = "get_module_fields";
+    public static final String GET_AVAILABLE_MODULES = "get_available_modules";
 
-	// params for the input JSON
-	public static final String USER_AUTH = "user_auth";
+    public static final String GET_ENTRY_LIST = "get_entry_list";
 
-	public static final String USER_NAME = "user_name";
+    public static final String GET_MODULE_FIELDS = "get_module_fields";
 
-	public static final String PASSWORD = "password";
+    // params for the input JSON
+    public static final String USER_AUTH = "user_auth";
 
-	public static final String SESSION = "session";
-	
-	public static final String MODULE_NAME = "module_name";
+    public static final String USER_NAME = "user_name";
 
-	// params for the output JSON
-	public static final String ID = "id";
+    public static final String PASSWORD = "password";
 
-	public static final String MODULES = "modules";
+    public static final String SESSION = "session";
 
-	public static final String ENTRY_LIST = "entry_list";
+    public static final String MODULE_NAME = "module_name";
+
+    // params for the output JSON
+    public static final String ID = "id";
+
+    public static final String MODULES = "modules";
+
+    public static final String MODULE_FIELDS = "module_fields";
+
+    public static final String ENTRY_LIST = "entry_list";
 
 }
