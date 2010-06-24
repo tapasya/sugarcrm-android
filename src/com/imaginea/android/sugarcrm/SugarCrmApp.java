46dd3818e2cf42cbf08fb6423ba402725db78abb
vasavin <vasavin>
2010-06-24 05:50:49 +0000

    modified the getter and setter methods to remove the suffix 'm'

--- src/com/imaginea/android/sugarcrm/SugarCrmApp.java
+++ src/com/imaginea/android/sugarcrm/SugarCrmApp.java
@@ -4,20 +4,19 @@ import android.app.Application;
 
 public class SugarCrmApp extends Application {
 
-	/*
-	 * sessionId is obtained after successful login into the Sugar CRM instance Now, sessionId will
-	 * be available to the entire application Access the sessionId from any part of the application
-	 * as follows : SugarCrmApp app = ((SugarCrmApp) getApplicationContext()); 
-	 * app.getmSessionId();
-	 */
-	private String mSessionId;
+    /*
+     * sessionId is obtained after successful login into the Sugar CRM instance Now, sessionId will
+     * be available to the entire application Access the sessionId from any part of the application
+     * as follows : SugarCrmApp app = ((SugarCrmApp) getApplicationContext()); app.getmSessionId();
+     */
+    private String mSessionId;
 
-	public String getmSessionId() {
-		return mSessionId;
-	}
+    public String getSessionId() {
+        return mSessionId;
+    }
 
-	public void setmSessionId(String mSessionId) {
-		this.mSessionId = mSessionId;
-	}
+    public void setSessionId(String mSessionId) {
+        this.mSessionId = mSessionId;
+    }
 
 }
