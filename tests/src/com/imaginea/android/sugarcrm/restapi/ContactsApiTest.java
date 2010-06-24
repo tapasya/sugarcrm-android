3e48fa5389cfbd4d1d64e5d2017fe4c0f925cf69
chanderp <chanderp>
2010-06-24 06:33:37 +0000

    initial version

---
+++ tests/src/com/imaginea/android/sugarcrm/restapi/ContactsApiTest.java
@@ -0,0 +1,12 @@
+package com.imaginea.android.sugarcrm.restapi;
+
+import android.test.suitebuilder.annotation.SmallTest;
+import android.util.Log;
+
+public class ContactsApiTest extends RestAPITest {
+	
+	@SmallTest
+	public void testContactsList() throws Exception {
+		Log.d("Test", "hello");
+	}
+}
