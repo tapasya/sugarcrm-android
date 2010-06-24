af07df81fc143e1cfc173fd7b0811f9a42a2a4d2
chanderp <chanderp>
2010-06-24 06:50:39 +0000

    tests for getting all fields of Contacts Module and custom dummy fields

--- tests/src/com/imaginea/android/sugarcrm/restapi/ContactsApiTest.java
+++ tests/src/com/imaginea/android/sugarcrm/restapi/ContactsApiTest.java
@@ -1,10 +1,33 @@
 package com.imaginea.android.sugarcrm.restapi;
 
+import com.imaginea.android.sugarcrm.util.RestUtil;
+
 import android.test.suitebuilder.annotation.SmallTest;
 import android.util.Log;
 
+/**
+ * ContactsApiTest, tests the rest api calls
+ * 
+ * @author chander
+ *
+ */
 public class ContactsApiTest extends RestAPITest {
+	String moduleName = "Contacts";
+	String[] fields = new String[] {};
+	String[] customFields = new String[]{"a","b"};
+
+	@SmallTest
+	public void testGetAllModuleFields() throws Exception {
+
+		RestUtil.getModuleFields(url, mSessionId, moduleName, fields);
+	}
 	
+	@SmallTest
+	public void testGetCustomModuleFields() throws Exception {
+
+		RestUtil.getModuleFields(url, mSessionId, moduleName, customFields);
+	}
+
 	@SmallTest
 	public void testContactsList() throws Exception {
 		Log.d("Test", "hello");
