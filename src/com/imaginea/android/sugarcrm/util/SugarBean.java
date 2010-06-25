381657a60c69e4fbb8ec05ff863e89c794bca346
vasavin <vasavin>
2010-06-25 08:47:48 +0000

    *** empty log message ***

---
+++ src/com/imaginea/android/sugarcrm/util/SugarBean.java
@@ -0,0 +1,29 @@
+package com.imaginea.android.sugarcrm.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SugarBean {
+
+    private String beanId;
+    private Map<String, String> entryList = new HashMap<String, String>();
+    
+    public String getBeanId() {
+        return beanId;
+    }
+    public void setBeanId(String beanId) {
+        this.beanId = beanId;
+    }
+    
+    public Map<String, String> getEntryList() {
+        return entryList;
+    }
+    public void setEntryList(Map<String, String> map) {
+        this.entryList = map;
+    }
+    
+    public String getFieldValue(String fieldName){
+        return entryList.get(fieldName);
+    }
+    
+}
