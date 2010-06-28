b824197efe55601d2ca2ff12d95323189293043e
vasavin <vasavin>
2010-06-28 15:35:05 +0000

    Added a constructor to take JSONResponse as a parameter. Updated to use SBParseHelper to parse the JSON Response.

--- src/com/imaginea/android/sugarcrm/util/SugarBean.java
+++ src/com/imaginea/android/sugarcrm/util/SugarBean.java
@@ -1,5 +1,12 @@
 package com.imaginea.android.sugarcrm.util;
 
+import static com.imaginea.android.sugarcrm.RestUtilConstants.ENTRY_LIST;
+import static com.imaginea.android.sugarcrm.RestUtilConstants.JSON_EXCEPTION;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,6 +15,25 @@ public class SugarBean {
     private String beanId;
 
     private Map<String, String> entryList = new HashMap<String, String>();
+
+    public SugarBean() {
+    }
+
+    public SugarBean(String jsonResponse) throws SugarCrmException {
+        try {
+            JSONObject responseObj = new JSONObject(jsonResponse);
+            JSONArray entryListJson = responseObj.getJSONArray(ENTRY_LIST);
+
+            // TODO: relationship_list also has to be looked at
+            // JSONArray mRelationshipListJson = responseObj.getJSONArray(RELATIONSHIP_LIST);
+
+            JSONObject jsonObject = (JSONObject) entryListJson.get(0);
+            setBeanId(jsonObject.get("id").toString());
+            setEntryList(SBParseHelper.getNameValuePairs(jsonObject));
+        } catch (JSONException e) {
+            throw new SugarCrmException(JSON_EXCEPTION, e.getMessage());
+        }
+    }
 
     public String getBeanId() {
         return beanId;
