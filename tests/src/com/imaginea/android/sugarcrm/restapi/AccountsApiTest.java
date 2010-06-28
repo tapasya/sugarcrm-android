0a45d0bffe55b8672c688fb71f06652b99419b20
vasavin <vasavin>
2010-06-28 15:43:42 +0000

    updated with test for getEntry

--- tests/src/com/imaginea/android/sugarcrm/restapi/AccountsApiTest.java
+++ tests/src/com/imaginea/android/sugarcrm/restapi/AccountsApiTest.java
@@ -1,27 +1,56 @@
 package com.imaginea.android.sugarcrm.restapi;
 
 import android.test.suitebuilder.annotation.SmallTest;
-import android.util.Log;
 
 import com.imaginea.android.sugarcrm.util.RestUtil;
-import com.imaginea.android.sugarcrm.util.SBList;
+import com.imaginea.android.sugarcrm.util.SBParser;
 import com.imaginea.android.sugarcrm.util.SugarBean;
 
-public class AccountsApiTest extends RestAPITest{
+public class AccountsApiTest extends RestAPITest {
 	String moduleName = "Accounts";
-	
+
 	@SmallTest
-	public void testGetEntryList() throws Exception{
+	public void testGetEntryList() throws Exception {
 		String query = "", orderBy = "";
 		int offset = 0, maxResults = 5, deleted = 0;
-		String[] selectFields = {"name", "email1"};
+		String[] selectFields = { "name", "email1" };
 		String[] linkNameToFieldsArray = {};
-		
-		SBList sbList = RestUtil.getEntryList(url, mSessionId, moduleName, query, orderBy, offset, selectFields, linkNameToFieldsArray, maxResults, deleted);
-		SugarBean[] sBeans = sbList.getSBEntryList();
-		for(SugarBean sBean : sBeans){
+
+		SBParser sbParser = RestUtil.getEntryList(url, mSessionId, moduleName,
+				query, orderBy, offset, selectFields, linkNameToFieldsArray,
+				maxResults, deleted);
+		SugarBean[] sBeans = sbParser.getSBEntryList();
+		for (SugarBean sBean : sBeans) {
 			System.out.println(sBean.getBeanId());
 			System.out.println(sBean.getFieldValue("email1"));
+		}
+	}
+
+	@SmallTest
+	public void testGetEntry() throws Exception {
+		String beanId = "1e9d5cb4-1972-28a4-7b36-4c1f261afd48";
+		String[] selectFields = { "name", "email1" };
+		String[] linkNameToFieldsArray = {};
+
+		SugarBean sBean = RestUtil.getEntry(url, mSessionId, moduleName,
+				beanId, selectFields, linkNameToFieldsArray);
+		System.out.println("Account Name : " + sBean.getFieldValue("name"));
+		System.out.println("Account email : " + sBean.getFieldValue("email1"));
+	}
+
+	@SmallTest
+	public void testGetEntryListWithNoSelectFields() throws Exception {
+		String query = "", orderBy = "";
+		int offset = 0, maxResults = 5, deleted = 0;
+		String[] selectFields = {};
+		String[] linkNameToFieldsArray = {};
+
+		SBParser sbParser = RestUtil.getEntryList(url, mSessionId, moduleName,
+				query, orderBy, offset, selectFields, linkNameToFieldsArray,
+				maxResults, deleted);
+		SugarBean[] sBeans = sbParser.getSBEntryList();
+		for (SugarBean sBean : sBeans) {
+			System.out.println(sBean.getBeanId());
 		}
 	}
 }
