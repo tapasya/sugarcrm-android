3134f580b71aa0edc130437fce87a0aebba42bad
vasavin <vasavin>
2010-07-01 08:29:23 +0000

    removed savedSettings map

--- src/com/imaginea/android/sugarcrm/SugarCrmSettings.java
+++ src/com/imaginea/android/sugarcrm/SugarCrmSettings.java
@@ -8,12 +8,7 @@ import android.util.Log;
 
 import com.imaginea.android.sugarcrm.util.Util;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class SugarCrmSettings extends PreferenceActivity {
-
-    private static final Map<String, String> savedSettings = new HashMap<String, String>();
 
     private static final String LOG_TAG = "SugarCrmSettings";
 
@@ -23,58 +18,7 @@ public class SugarCrmSettings extends PreferenceActivity {
         addPreferencesFromResource(R.xml.sugarcrm_settings);
     }
 
-    /**
-     * This methods saves the current settings, to be able to check later if settings changed
-     * 
-     * @param context
-     */
-    public static void saveCurrentSettings(Context context) {
-        savedSettings.clear();
-        savedSettings.put(Util.PREF_REST_URL, getSugarRestUrl(context));
-        savedSettings.put(Util.PREF_USERNAME, getUsername(context));
-        savedSettings.put(Util.PREF_PASSWORD, getPassword(context));
-    }
-
-    /**
-     * This methods tells is the settings changed. It is used by a callback called when resuming
-     * from the ConnectionSettingsActivity
-     * 
-     * @param context
-     *            an activity
-     * @return
-     */
-    public static boolean currentSettingsChanged(Context context) {
-
-        if (savedSettings.isEmpty()) {
-            return false;
-        }
-
-        try {
-            if (!getSugarRestUrl(context).equals(savedSettings.get(Util.PREF_REST_URL))) {
-                return true;
-            }
-
-            if (!getUsername(context).equals(savedSettings.get(Util.PREF_USERNAME))) {
-                return true;
-            }
-
-            if (!getPassword(context).equals(savedSettings.get(Util.PREF_PASSWORD))) {
-                return true;
-            }
-
-            // if (!isPasswordSaved(context).equals(savedSettings.get(Util.PREF_REMEMBER_PASSWORD)))
-            // {
-            // return true;
-            // }
-
-            return false;
-        } finally {
-            savedSettings.clear();
-        }
-    }
-
     // Static getters (extracting data from context)
-
     public static String getUsername(Context context) {
         return PreferenceManager.getDefaultSharedPreferences(context).getString(Util.PREF_USERNAME, context.getString(R.string.default_username));
     }
