80363d7735863be9d87d31c378075faa66f226e4
chanderp <chanderp>
2010-07-05 09:31:10 +0000

    formatted

--- tests/src/com/imaginea/android/sugarcrm/restapi/RestAPITest.java
+++ tests/src/com/imaginea/android/sugarcrm/restapi/RestAPITest.java
@@ -1,50 +1,51 @@
 package com.imaginea.android.sugarcrm.restapi;
 
-import java.util.List;
-
-import junit.framework.TestCase;
 import android.test.AndroidTestCase;
-import android.test.suitebuilder.annotation.SmallTest;
 
 import com.imaginea.android.sugarcrm.util.RestUtil;
+
+import junit.framework.TestCase;
 
 /**
  * 
  * Extend this class for module specific unit tests.
  * 
- * Since this test doesn't need a {@link android.content.Context}, or any other
- * dependencies injected, it simply extends the standard {@link TestCase}.
+ * Since this test doesn't need a {@link android.content.Context}, or any other dependencies
+ * injected, it simply extends the standard {@link TestCase}.
  * 
- * See {@link com.imaginea.android.sugarcrm.AllTests} for documentation on
- * running all tests and individual tests in this application.
+ * See {@link com.imaginea.android.sugarcrm.AllTests} for documentation on running all tests and
+ * individual tests in this application.
  */
 public class RestAPITest extends AndroidTestCase {
 
-	/**
-	 * used by module specific unit tests
-	 */
-	protected String url = "http://192.168.2.245/sugarcrm/service/v2/rest.php";
-	protected String userName = "will";
-	protected String password = "will";
-	protected String mSessionId;
+    /**
+     * used by module specific unit tests
+     */
+    protected String url = "http://192.168.2.245/sugarcrm/service/v2/rest.php";
 
-	@Override
-	protected void setUp() throws Exception {
+    protected String userName = "will";
 
-		super.setUp();
-		authenticate();
-	}
+    protected String password = "will";
 
-	public void authenticate() throws Exception {
+    protected String mSessionId;
 
-		mSessionId = RestUtil.loginToSugarCRM(url, userName, password);
-		assertNotNull(mSessionId);
-	}
+    @Override
+    protected void setUp() throws Exception {
 
-	/*
-	 * @SmallTest public void testGetModules() throws Exception{ List<String>
-	 * modules = RestUtil.getAvailableModules(url, mSessionId); for(String
-	 * module : modules){ System.out.print("\t" + module); } }
-	 */
+        super.setUp();
+        authenticate();
+    }
+
+    public void authenticate() throws Exception {
+
+        mSessionId = RestUtil.loginToSugarCRM(url, userName, password);
+        assertNotNull(mSessionId);
+    }
+
+    /*
+     * @SmallTest public void testGetModules() throws Exception{ List<String> modules =
+     * RestUtil.getAvailableModules(url, mSessionId); for(String module : modules){
+     * System.out.print("\t" + module); } }
+     */
 
 }
