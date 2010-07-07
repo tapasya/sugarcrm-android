4290123ef2b8609e4bbaf63f6e194b4122f71ea6
chanderp <chanderp>
2010-07-07 13:53:54 +0000

    initial version

---
+++ src/com/imaginea/android/sugarcrm/util/CRMCustomLogFormatter.java
@@ -0,0 +1,43 @@
+package com.imaginea.android.sugarcrm.util;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
+
+/**
+ * CRMCustomLogFormatter formats the LogRecord as follows: date level localized message with
+ * parameters
+ */
+public class CRMCustomLogFormatter extends Formatter {
+
+    DateFormat mDateFormat;
+
+    public CRMCustomLogFormatter() {
+        super();
+        mDateFormat = DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.SHORT);
+    }
+
+    public String format(LogRecord record) {
+
+        // Create a StringBuffer to contain the formatted record
+        // start with the date.
+        StringBuffer sb = new StringBuffer();
+
+        // Get the date from the LogRecord and add it to the buffer
+        Date date = new Date(record.getMillis());
+        sb.append(mDateFormat.format(date.getTime()));
+        sb.append(" ");
+
+        // Get the level name and add it to the buffer
+        sb.append(record.getLevel().getName());
+        sb.append(" ");
+
+        // Get the formatted message (includes localization
+        // and substitution of paramters) and add it to the buffer
+        sb.append(formatMessage(record));
+        sb.append("\n");
+
+        return sb.toString();
+    }
+}
