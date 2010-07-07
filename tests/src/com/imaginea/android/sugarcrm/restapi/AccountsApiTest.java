707be19b77ecec1454d38e163863465543661d06
vasavin <vasavin>
2010-07-07 12:47:04 +0000

    added test cases for getRelationships, setRelationships and setRelationship

--- tests/src/com/imaginea/android/sugarcrm/restapi/AccountsApiTest.java
+++ tests/src/com/imaginea/android/sugarcrm/restapi/AccountsApiTest.java
@@ -5,10 +5,13 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.json.JSONObject;
+
 import android.test.suitebuilder.annotation.SmallTest;
 import android.util.Log;
 
 import com.imaginea.android.sugarcrm.ModuleFields;
+import com.imaginea.android.sugarcrm.RestUtilConstants;
 import com.imaginea.android.sugarcrm.util.RestUtil;
 import com.imaginea.android.sugarcrm.util.SugarBean;
 
@@ -62,37 +65,98 @@ public class AccountsApiTest extends RestAPITest {
 				beanId, selectFields, linkNameToFieldsArray);
 		System.out.println("Account Name : " + sBean.getFieldValue("name"));
 		System.out.println("Account email : " + sBean.getFieldValue("email1"));
-		System.out.println("Phone office : " + sBean.getFieldValue(ModuleFields.PHONE_OFFICE));
-		System.out.println("Account deleted ? " + sBean.getFieldValue(ModuleFields.DELETED));
+		System.out.println("Phone office : "
+				+ sBean.getFieldValue(ModuleFields.PHONE_OFFICE));
+		System.out.println("Account deleted ? "
+				+ sBean.getFieldValue(ModuleFields.DELETED));
 	}
-	
+
 	@SmallTest
-	public void testSetEntry() throws Exception{
+	public void testSetEntry() throws Exception {
 		String beanId = "1e9d5cb4-1972-28a4-7b36-4c1f261afd48";
 		Map<String, String> nameValuePairs = new LinkedHashMap<String, String>();
 		nameValuePairs.put(ModuleFields.ID, beanId);
 		nameValuePairs.put(ModuleFields.NAME, "R R Advertising Inc.");
-		//nameValuePairs.put(ModuleFields.PHONE_OFFICE, "(078) 123-4567");
-		String _beanId = RestUtil.setEntry(url, mSessionId, moduleName, nameValuePairs);
+		// nameValuePairs.put(ModuleFields.PHONE_OFFICE, "(078) 123-4567");
+		String _beanId = RestUtil.setEntry(url, mSessionId, moduleName,
+				nameValuePairs);
 		System.out.println("setEntry response : " + _beanId);
 		assertNotNull(_beanId);
 	}
-	
+
 	@SmallTest
-	public void testSetEntries() throws Exception{
+	public void testSetEntries() throws Exception {
 		List<Map<String, String>> beanNameValuePairs = new ArrayList<Map<String, String>>();
-		
+
 		String beanId = "1e9d5cb4-1972-28a4-7b36-4c1f261afd48";
 		Map<String, String> nameValuePairs = new LinkedHashMap<String, String>();
 		nameValuePairs.put(ModuleFields.ID, beanId);
 		nameValuePairs.put(ModuleFields.NAME, "R Advertising Inc.");
-		//nameValuePairs.put(ModuleFields.PHONE_OFFICE, "(078) 123-4567");
-		
+		// nameValuePairs.put(ModuleFields.PHONE_OFFICE, "(078) 123-4567");
+
 		beanNameValuePairs.add(nameValuePairs);
-		List<String> beanIds = RestUtil.setEntries(url, mSessionId, moduleName, beanNameValuePairs);
-		for(String _beanId : beanIds){
+		List<String> beanIds = RestUtil.setEntries(url, mSessionId, moduleName,
+				beanNameValuePairs);
+		for (String _beanId : beanIds) {
 			System.out.println(_beanId);
 		}
+	}
+
+	@SmallTest
+	public void testGetRelationships() throws Exception {
+		String beanId = "1e9d5cb4-1972-28a4-7b36-4c1f261afd48";
+		String linkFieldName = "Contacts";
+		String relatedModuleQuery = "";
+		String[] relatedFields = { "name" };
+		String[] relatedModuleLinkNameToFieldsArray = { "id", "name", "email1" };
+		int deleted = 0;
+		String response = RestUtil.getRelationships(url, mSessionId,
+				moduleName, beanId, linkFieldName, relatedModuleQuery,
+				relatedFields, relatedModuleLinkNameToFieldsArray, deleted);
+		JSONObject jsonResponse = new JSONObject(response);
+		assertNotNull(jsonResponse.get(RestUtilConstants.ENTRY_LIST).toString());
+		assertNotNull(jsonResponse.get(RestUtilConstants.RELATIONSHIP_LIST)
+				.toString());
+	}
+
+	@SmallTest
+	public void testSetRelationship() throws Exception {
+		String beanId = "1e9d5cb4-1972-28a4-7b36-4c1f261afd48";
+		String linkFieldName = "Contacts";
+		String[] relatedIds = {};
+		Map<String, String> nameValueList = new LinkedHashMap<String, String>();
+		nameValueList.put(ModuleFields.ID, beanId);
+		int delete = 0;
+		String response = RestUtil.setRelationship(url, mSessionId, moduleName,
+				beanId, linkFieldName, relatedIds, nameValueList, delete);
+		JSONObject jsonResponse = new JSONObject(response);
+		assertNotNull(jsonResponse.get(RestUtilConstants.CREATED).toString());
+		assertNotNull(jsonResponse.get(RestUtilConstants.FAILED).toString());
+		assertNotNull(jsonResponse.get(RestUtilConstants.DELETED).toString());
+	}
+
+	@SmallTest
+	public void testSetRelationships() throws Exception {
+		List<Map<String, String>> nameValueLists = new ArrayList<Map<String, String>>();
+
+		String[] moduleNames = { "Contacts" };
+		String[] beanIds = { "1e9d5cb4-1972-28a4-7b36-4c1f261afd48" };
+		String[] linkFieldNames = { "" };
+		String[] relatedIds = {};
+
+		Map<String, String> nameValueList = new LinkedHashMap<String, String>();
+		nameValueList.put(ModuleFields.ID, beanIds[0]);
+
+		int deleted = 0;
+
+		nameValueLists.add(nameValueList);
+		String response = RestUtil.setRelationships(url, mSessionId,
+				moduleNames, beanIds, linkFieldNames, relatedIds,
+				nameValueLists, deleted);
+		JSONObject jsonResponse = new JSONObject(response);
+		assertNotNull(jsonResponse.get(RestUtilConstants.CREATED).toString());
+		assertNotNull(jsonResponse.get(RestUtilConstants.FAILED).toString());
+		assertNotNull(jsonResponse.get(RestUtilConstants.DELETED).toString());
 	}
 
 	/*
@@ -108,16 +172,14 @@ public class AccountsApiTest extends RestAPITest {
 	 */
 
 	/*
-	public void testGetModuleFields() throws Exception {
-		List<ModuleField> moduleFields = RestUtil.getModuleFields(url,
-				mSessionId, moduleName, selectFields);
-		for (ModuleField moduleField : moduleFields) {
-			System.out.println("name :" + moduleField.getName());
-			System.out.println("label :" + moduleField.getName());
-			System.out.println("type :" + moduleField.getName());
-			System.out.println("isReuired :" + moduleField.ismIsRequired());
-		}
-	}*/
+	 * public void testGetModuleFields() throws Exception { List<ModuleField>
+	 * moduleFields = RestUtil.getModuleFields(url, mSessionId, moduleName,
+	 * selectFields); for (ModuleField moduleField : moduleFields) {
+	 * System.out.println("name :" + moduleField.getName());
+	 * System.out.println("label :" + moduleField.getName());
+	 * System.out.println("type :" + moduleField.getName());
+	 * System.out.println("isReuired :" + moduleField.ismIsRequired()); } }
+	 */
 
 	/**
 	 * demonstrates the usage of RestUtil for contacts List. ModuleFields.NAME
