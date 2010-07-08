34b05b08b13f9a3d263a9ba5ac0caed116344c2a
vasavin <vasavin>
2010-07-08 13:15:10 +0000

    modified so that the ocde can be re-used for parsing both the entry list and the search result

--- src/com/imaginea/android/sugarcrm/util/SBParseHelper.java
+++ src/com/imaginea/android/sugarcrm/util/SBParseHelper.java
@@ -1,6 +1,6 @@
 package com.imaginea.android.sugarcrm.util;
 
-import static com.imaginea.android.sugarcrm.RestUtilConstants.EXCEPTION;
+import static com.imaginea.android.sugarcrm.RestUtilConstants.VALUE;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -15,26 +15,27 @@ public class SBParseHelper {
      * Helper for the SBParser and SugarBean to parse the JSON response to retrieve the name value
      * pairs either in the entry_list or the relationship_list
      */
-    public static Map<String, String> getNameValuePairs(JSONObject jsonObject)
+    public static Map<String, String> getNameValuePairs(String nameValueList)
                                     throws SugarCrmException {
         Map<String, String> fields = new HashMap<String, String>();
         try {
-            String nameValueList = jsonObject.get("name_value_list").toString();
             JSONObject nameVal = new JSONObject(nameValueList);
             Iterator iter = nameVal.keys();
             while (iter.hasNext()) {
                 String key = (String) iter.next();
-                String val = ((JSONObject) (nameVal.get(key))).get("value").toString();
+                String val = ((JSONObject) (nameVal.get(key))).get(VALUE).toString();
                 fields.put(key, val);
             }
             return fields;
+            
         } catch (JSONException e) {
             /*
              * when the select_fields is empty while making the rest call, the default response will
              * give empty JSONArray and hence it throws an exception
              */
-            throw new SugarCrmException(EXCEPTION, "No name value pairs available!");
+            throw new SugarCrmException("No name value pairs available!");
         }
 
     }
+
 }
