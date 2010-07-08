a47dccaec4b02c8fa786435bc5feb0ea0b47bd8d
vasavin <vasavin>
2010-07-08 13:03:45 +0000

    organized imports

--- src/com/imaginea/android/sugarcrm/WizardActivity.java
+++ src/com/imaginea/android/sugarcrm/WizardActivity.java
@@ -20,7 +20,6 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.CheckBox;
