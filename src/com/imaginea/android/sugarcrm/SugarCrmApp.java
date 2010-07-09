ce1eb2de1df913a73015a048fc1c5db9102e270a
chanderp <chanderp>
2010-07-09 09:48:49 +0000

    moved the login code and added a ref to this for easy access of this class to other components

--- src/com/imaginea/android/sugarcrm/SugarCrmApp.java
+++ src/com/imaginea/android/sugarcrm/SugarCrmApp.java
@@ -1,9 +1,15 @@
 package com.imaginea.android.sugarcrm;
 
 import android.app.Application;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
 
 public class SugarCrmApp extends Application {
 
+    // easy ref to App instance for classes which do not have access to Activity/Service context
+    public static Application app = null;
+    
     /*
      * sessionId is obtained after successful login into the Sugar CRM instance Now, sessionId will
      * be available to the entire application Access the sessionId from any part of the application
@@ -12,11 +18,26 @@ public class SugarCrmApp extends Application {
     private String mSessionId;
 
     public String getSessionId() {
+        // TODO - remove this
+        if (mSessionId == null) {
+            SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+            // TODO use a constant and remove this as we start from the login screen
+            String url = pref.getString("URL", getBaseContext().getString(R.string.defaultUrl));
+            String userName = pref.getString("USER_NAME", getBaseContext().getString(R.string.defaultUser));
+            String password = pref.getString("PASSWORD", getBaseContext().getString(R.string.defaultPwd));
+            Log.i("SugarAPP", url + userName + password);
+        }
         return mSessionId;
     }
 
     public void setSessionId(String mSessionId) {
         this.mSessionId = mSessionId;
+    }
+
+    @Override
+    public void onCreate() {        
+        super.onCreate();
+        app = this;
     }
 
 }
