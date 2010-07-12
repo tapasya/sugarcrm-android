dd4dd1e8e941c367eb13951938c25fc81c5147b5
chanderp <chanderp>
2010-07-12 06:25:25 +0000

    added helpers for delete and update

--- src/com/imaginea/android/sugarcrm/ServiceHelper.java
+++ src/com/imaginea/android/sugarcrm/ServiceHelper.java
@@ -6,6 +6,10 @@ import android.net.Uri;
 
 import com.imaginea.android.sugarcrm.util.Util;
 
+import java.io.Serializable;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /**
  * ServiceHelper
  * 
@@ -18,9 +22,34 @@ public class ServiceHelper {
         // send a notify command to the service
         Intent serviceIntent = new Intent(context, SugarService.class);
         serviceIntent.setData(uri);
+        serviceIntent.putExtra(Util.COMMAND, R.id.get);
         serviceIntent.putExtra(RestUtilConstants.MODULE_NAME, module);
         serviceIntent.putExtra(Util.PROJECTION, projection);
         serviceIntent.putExtra(Util.SORT_ORDER, sortOrder);
+        context.startService(serviceIntent);
+    }
+
+    public static void startServiceForDelete(Context context, Uri uri, String module, String beanId) {
+        Intent serviceIntent = new Intent(context, SugarService.class);
+        serviceIntent.setData(uri);
+        Map<String, String> nameValuePairs = new LinkedHashMap<String, String>();
+        nameValuePairs.put(ModuleFields.ID, beanId);
+        nameValuePairs.put(ModuleFields.DELETED, "1");
+
+        serviceIntent.putExtra(Util.COMMAND, R.id.update);
+        serviceIntent.putExtra(RestUtilConstants.MODULE_NAME, module);
+        serviceIntent.putExtra(RestUtilConstants.NAME_VALUE_LIST, (Serializable) nameValuePairs);
+        context.startService(serviceIntent);
+    }
+
+    public static void startServiceForUpdate(Context context, Uri uri, String module,
+                                    String beanId, Map nameValueList) {
+        Intent serviceIntent = new Intent(context, SugarService.class);
+        serviceIntent.setData(uri);
+
+        serviceIntent.putExtra(Util.COMMAND, R.id.update);
+        serviceIntent.putExtra(RestUtilConstants.MODULE_NAME, module);
+        serviceIntent.putExtra(RestUtilConstants.NAME_VALUE_LIST, (Serializable) nameValueList);
         context.startService(serviceIntent);
     }
 }
