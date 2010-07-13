7f48cb636a4d5f90b2f3501623ed42197b4c3299
chanderp <chanderp>
2010-07-13 11:57:12 +0000

    initial version Both WizardActivity and WizardAuthActivity need to be kept in sync, but we may decide to drop support for 1.6 and remove WizardActivity if it becomes unmaintainable, keep it for now.

---
+++ src/com/imaginea/android/sugarcrm/WizardDetector.java
@@ -0,0 +1,26 @@
+package com.imaginea.android.sugarcrm;
+
+import android.content.Context;
+import android.os.Build;
+
+/**
+ * VersionedWizardDetector, is solely present to make the Account login work on both 1.6 and 2.0
+ * 
+ */
+public abstract class WizardDetector {
+    public static Class getClass(Context context) {
+        final int sdkVersion = Build.VERSION.SDK_INT;
+        Class wizardClass = WizardActivity.class;
+        if (sdkVersion < Build.VERSION_CODES.ECLAIR) {
+            wizardClass = WizardActivity.class;
+        } else {
+            try {
+                wizardClass = Class.forName("com.imaginea.android.sugarcrm.WizardAuthActivity");
+            } catch (ClassNotFoundException ce) {
+                ce.printStackTrace();
+            }
+        }
+
+        return wizardClass;
+    }
+}
