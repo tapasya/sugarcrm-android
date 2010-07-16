e03644ea47d5aba6297b5b27c6ef932a6ceca47b
vasavin <vasavin>
2010-07-16 10:09:49 +0000

    added menu and modified to re-use the same code ((based on the intent.getData())) to show the details of a bean for relationships as well. commented the Relationship adapter code as it is pushed into the menu.

--- src/com/imaginea/android/sugarcrm/AccountDetailsActivity.java
+++ src/com/imaginea/android/sugarcrm/AccountDetailsActivity.java
@@ -6,13 +6,13 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.text.util.Linkify;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.SubMenu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -20,11 +20,14 @@ import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TableLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.imaginea.android.sugarcrm.provider.DatabaseHelper;
 import com.imaginea.android.sugarcrm.util.ModuleField;
-import com.imaginea.android.sugarcrm.util.SugarBean;
 import com.imaginea.android.sugarcrm.util.Util;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * AccountDetailsActivity
@@ -34,22 +37,22 @@ import com.imaginea.android.sugarcrm.util.Util;
 public class AccountDetailsActivity extends Activity {
 
     private Menu mMenu;
-    
+
     private String mRowId;
-    
+
     private String mSugarBeanId;
-    
+
     private String mModuleName;
 
-    private Cursor mCursor;    
+    private Cursor mCursor;
 
     private String[] mSelectFields;
 
     private final String LOG_TAG = "AccountDetailsActivity";
 
     private TableLayout mDetailsTable;
-    
-    private String mAccountName = "";
+
+    private String[] mRelationshipModules;
 
     /** Called when the activity is first created. */
     @Override
@@ -74,51 +77,55 @@ public class AccountDetailsActivity extends Activity {
         mCursor = getContentResolver().query(getIntent().getData(), mSelectFields, null, null, DatabaseHelper.getModuleSortOrder(mModuleName));
         // startManagingCursor(mCursor);
         setContents(mModuleName);
-        
-        final String[] relationshipModules = DatabaseHelper.getModuleRelationshipItems(mModuleName);
-        
-        ListView listView = (ListView)findViewById(android.R.id.list);
+
+        mRelationshipModules = DatabaseHelper.getModuleRelationshipItems(mModuleName);
+
+        ListView listView = (ListView) findViewById(android.R.id.list);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> arg0, View view, int position, long id) {
-                Log.i(LOG_TAG, "clicked on " + relationshipModules[position]);
-                openListScreen(relationshipModules[position]);
+                if (Log.isLoggable(LOG_TAG, Log.INFO)) {
+                    Log.i(LOG_TAG, "clicked on " + mRelationshipModules[position]);
+                }
+                openListScreen(mRelationshipModules[position]);
             }
         });
-        
-        RelationshipAdapter adapter = new RelationshipAdapter(this);
-        adapter.setRelationshipArray(relationshipModules);
-        listView.setAdapter(adapter);
-        
+
+        /*RelationshipAdapter adapter = new RelationshipAdapter(this);
+        adapter.setRelationshipArray(mRelationshipModules);
+        listView.setAdapter(adapter);*/
+
     }
 
     protected void openListScreen(String moduleName) {
-        Intent detailIntent = new Intent(AccountDetailsActivity.this, ContactListActivity.class);
-        Uri uri = Uri.withAppendedPath(DatabaseHelper.getModuleUri(mModuleName), mRowId);
-        uri = Uri.withAppendedPath(uri, DatabaseHelper.getPathForRelationship(moduleName));
-        detailIntent.setData(uri);
-        detailIntent.putExtra(RestUtilConstants.PARENT_MODULE_NAME, mModuleName);
-        detailIntent.putExtra(RestUtilConstants.MODULE_NAME, moduleName);
-        detailIntent.putExtra(RestUtilConstants.BEAN_ID, mSugarBeanId);
-        startActivity(detailIntent);
+        if (mModuleName.equals("Accounts")) {
+            Intent detailIntent = new Intent(AccountDetailsActivity.this, ContactListActivity.class);
+            Uri uri = Uri.withAppendedPath(DatabaseHelper.getModuleUri(mModuleName), mRowId);
+            uri = Uri.withAppendedPath(uri, DatabaseHelper.getPathForRelationship(moduleName));
+            detailIntent.setData(uri);
+            detailIntent.putExtra(RestUtilConstants.MODULE_NAME, moduleName);
+            detailIntent.putExtra(RestUtilConstants.BEAN_ID, mSugarBeanId);
+            startActivity(detailIntent);
+        } else {
+            Toast.makeText(this, "Not yet supported!", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        
-        if(mCursor != null && !mCursor.isClosed())
+
+        if (mCursor != null && !mCursor.isClosed())
             mCursor.close();
-        
-    }
-    
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        MenuHelper.onPrepareOptionsMenu(this, menu, mModuleName);
-        return super.onPrepareOptionsMenu(menu);
+
     }
 
-    
+    /*
+     * @Override public boolean onPrepareOptionsMenu(Menu menu) {
+     * MenuHelper.onPrepareOptionsMenu(this, menu, mModuleName); return
+     * super.onPrepareOptionsMenu(menu); }
+     */
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Hold on to this
@@ -126,91 +133,115 @@ public class AccountDetailsActivity extends Activity {
 
         // Inflate the currently selected menu XML resource.
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.list_activity_menu, menu);
-        
+        inflater.inflate(R.menu.details_activity_menu, menu);
+
+        SubMenu relationshipMenu = menu.addSubMenu(0, 123456, 0, "Related");
+        for (int i = 0; i < mRelationshipModules.length; i++) {
+            relationshipMenu.add(0, Menu.FIRST + i, 0, mRelationshipModules[i]);
+        }
+
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-        case R.id.addItem:
-            Intent myIntent = new Intent(AccountDetailsActivity.this, EditDetailsActivity.class);
-            myIntent.putExtra(Util.ROW_ID, mRowId);
-            myIntent.putExtra(RestUtilConstants.BEAN_ID, mSugarBeanId);
-            myIntent.putExtra(RestUtilConstants.LINK_FIELD_NAME, "contacts");
-            myIntent.putExtra(RestUtilConstants.MODULE_NAME, "Contacts");
-            myIntent.putExtra(RestUtilConstants.PARENT_MODULE_NAME, "Accounts");
-            myIntent.putExtra(ModuleFields.ACCOUNT_NAME, mAccountName);
+        case R.id.home:
+            Intent myIntent = new Intent(AccountDetailsActivity.this, DashboardActivity.class);
             AccountDetailsActivity.this.startActivity(myIntent);
             return true;
-
+        case 123456:
+            return true;
+        default:
+            if (Log.isLoggable(LOG_TAG, Log.INFO)){
+                Log.i(LOG_TAG, "item id : " + item.getItemId());
+            }
+            
+            if (mModuleName.equals("Accounts")) {
+                openListScreen(mRelationshipModules[item.getItemId() - 1]);
+            } else {
+                Toast.makeText(this, "Not yet supported!", Toast.LENGTH_SHORT).show();
+            }
+            return true;
         }
-        return false;
+        // return false;
     }
 
     private void setContents(String moduleName) {
 
         String[] detailsProjection = mSelectFields;
 
-        TextView textViewForAccountName = (TextView) findViewById(R.id.accountName);
+        TextView tv = (TextView) findViewById(R.id.headerText);
+        tv.setText(mModuleName + " Details");
+
+        TextView textViewForTitle = (TextView) findViewById(R.id.accountName);
+        String title = "";
+        List<String> titleFields = Arrays.asList(DatabaseHelper.getModuleListSelections(mModuleName));
+
         mDetailsTable = (TableLayout) findViewById(R.id.accountDetalsTable);
 
         mCursor.moveToFirst();
-        
+
         LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        
-        for (int i = 2; i < detailsProjection.length-2; i++) {
+
+        for (int i = 2; i < detailsProjection.length - 2; i++) {
             String fieldName = detailsProjection[i];
             int columnIndex = mCursor.getColumnIndex(fieldName);
-            Log.d(LOG_TAG, "Col:" + columnIndex + " moduleName : " + moduleName + " fieldName : " + fieldName );
-            
-            //TODO: get the attributes of the moduleField
+            if (Log.isLoggable(LOG_TAG, Log.INFO)) {
+                Log.d(LOG_TAG, "Col:" + columnIndex + " moduleName : " + moduleName
+                                                + " fieldName : " + fieldName);
+            }
+
+            // get the attributes of the moduleField
             ModuleField moduleField = DatabaseHelper.getModuleField(moduleName, fieldName);
-        
+
             View tableRow = inflater.inflate(R.layout.table_row, null);
-            TextView textViewForLabel = (TextView)tableRow.findViewById(R.id.detailRowLabel);
-            TextView textViewForValue = (TextView)tableRow.findViewById(R.id.detailRowValue);
-            
-            textViewForLabel.setText(moduleField.getLabel());
-            String value = mCursor.getString(columnIndex);
-            
-            if(ModuleFields.NAME.equals(fieldName)){
-                mAccountName = value;
-                textViewForAccountName.setText(value);
+            TextView textViewForLabel = (TextView) tableRow.findViewById(R.id.detailRowLabel);
+            TextView textViewForValue = (TextView) tableRow.findViewById(R.id.detailRowValue);
+
+            String value = "";
+            if (mCursor.getCount() > 0) {
+                value = mCursor.getString(columnIndex);
+            }
+
+            if (titleFields.contains(fieldName)) {
+                title = title + value + " ";
+                textViewForTitle.setText(title);
                 continue;
             }
-            
-            if(moduleField.getType().equals("phone"))
+
+            textViewForLabel.setText(moduleField.getLabel());
+
+            if (moduleField.getType().equals("phone"))
                 textViewForValue.setAutoLinkMask(Linkify.PHONE_NUMBERS);
             if (value != null && !value.equals("")) {
-                textViewForValue.setText(value);             
+                textViewForValue.setText(value);
             } else {
                 textViewForValue.setText(R.string.notAvailable);
             }
-            
+
             mDetailsTable.addView(tableRow);
 
         }
     }
-    
+
     private class RelationshipAdapter extends BaseAdapter {
 
         private Context mContext;
-        
+
         private String[] relationships;
-        
+
         private LayoutInflater mInflater;
-        
+
         public RelationshipAdapter(Context context) {
             mContext = context;
             mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         }
-        
+
         public void setRelationshipArray(String[] relationshipArray) {
             relationships = relationshipArray;
         }
-        
+
         @Override
         public int getCount() {
             return relationships.length;
@@ -228,12 +259,14 @@ public class AccountDetailsActivity extends Activity {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            TextView relationshipName = (TextView)mInflater.inflate(R.layout.listitem, null);
-            relationshipName.setText(relationships[position]);
-            return relationshipName;
+            View layout = mInflater.inflate(R.layout.contact_listitem, null);
+            TextView tv = ((TextView) layout.findViewById(android.R.id.text1));
+            tv.setText(relationships[position]);
+            // TODO: either set the correct images or remove the image
+            tv.setCompoundDrawablesWithIntrinsicBounds(getResources().getDrawable(R.drawable.contacts), null, null, null);
+            return layout;
         }
-        
+
     }
 
-    
 }
