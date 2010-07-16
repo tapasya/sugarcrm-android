0aadf0cccf11a37ab0b8a9fdc9c6bd0f45a3f0fc
chanderp <chanderp>
2010-07-16 08:25:15 +0000

    initial version

---
+++ src/com/imaginea/android/sugarcrm/SyncConfigActivity.java
@@ -0,0 +1,155 @@
+package com.imaginea.android.sugarcrm;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.DatePickerDialog.OnDateSetListener;
+import android.os.Bundle;
+import android.text.format.DateUtils;
+import android.text.format.Time;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+/**
+ * SyncConfigActivity
+ * 
+ * @author chander
+ * 
+ */
+public class SyncConfigActivity extends Activity {
+
+    private TextView mHeaderTextView;
+
+    private TextView mTitleTextView;
+
+    private Button mStartDateButton;
+
+    private Button mEndDateButton;
+
+    // cache the time
+    private Time mStartTime;
+
+    private Time mEndTime;
+
+    public static final String TAG = SyncConfigActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Setup layout
+        setContentView(R.layout.sync_config);
+        mHeaderTextView = (TextView) findViewById(R.id.headerText);
+        mHeaderTextView.setText(R.string.syncFilters);
+        // mTitleTextView = (TextView) findViewById(R.id.title);
+        mStartDateButton = (Button) findViewById(R.id.start_date);
+        mEndDateButton = (Button) findViewById(R.id.end_date);
+
+        mStartTime = new Time();
+        mEndTime = new Time();
+
+        populateWhen();
+
+    }
+
+    private void populateWhen() {
+        long startMillis = mStartTime.toMillis(false /* use isDst */);
+        long endMillis = mEndTime.toMillis(false /* use isDst */);
+        setDate(mStartDateButton, startMillis);
+        setDate(mEndDateButton, endMillis);
+
+        mStartDateButton.setOnClickListener(new DateClickListener(mStartTime));
+        mEndDateButton.setOnClickListener(new DateClickListener(mEndTime));
+    }
+
+    /**
+     * DateListener
+     */
+    private class DateListener implements OnDateSetListener {
+        View mView;
+
+        public DateListener(View view) {
+            mView = view;
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int monthDay) {
+            // Cache the member variables locally to avoid inner class overhead.
+            Time startTime = mStartTime;
+            Time endTime = mEndTime;
+
+            // Cache the start and end millis so that we limit the number
+            // of calls to normalize() and toMillis(), which are fairly
+            // expensive.
+            long startMillis;
+            long endMillis;
+            if (mView == mStartDateButton) {
+                // The start date was changed.
+                int yearDuration = endTime.year - startTime.year;
+                int monthDuration = endTime.month - startTime.month;
+                int monthDayDuration = endTime.monthDay - startTime.monthDay;
+
+                startTime.year = year;
+                startTime.month = month;
+                startTime.monthDay = monthDay;
+                startMillis = startTime.normalize(true);
+
+                // Also update the end date to keep the duration constant.
+                endTime.year = year + yearDuration;
+                endTime.month = month + monthDuration;
+                endTime.monthDay = monthDay + monthDayDuration;
+                endMillis = endTime.normalize(true);
+
+            } else {
+                // The end date was changed.
+                startMillis = startTime.toMillis(true);
+                endTime.year = year;
+                endTime.month = month;
+                endTime.monthDay = monthDay;
+                endMillis = endTime.normalize(true);
+
+                // Do not allow an event to have an end time before the start time.
+                if (endTime.before(startTime)) {
+                    endTime.set(startTime);
+                    endMillis = startMillis;
+                }
+            }
+
+            setDate(mStartDateButton, startMillis);
+            setDate(mEndDateButton, endMillis);
+
+        }
+    }
+
+    /**
+     * DateClickListener
+     */
+    private class DateClickListener implements View.OnClickListener {
+        private Time mTime;
+
+        public DateClickListener(Time time) {
+            mTime = time;
+        }
+
+        public void onClick(View v) {
+            new DatePickerDialog(SyncConfigActivity.this, new DateListener(v), mTime.year, mTime.month, mTime.monthDay).show();
+        }
+    }
+
+    private void setDate(TextView view, long millis) {
+        int flags = DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_SHOW_YEAR
+                                        | DateUtils.FORMAT_SHOW_WEEKDAY
+                                        | DateUtils.FORMAT_ABBREV_MONTH
+                                        | DateUtils.FORMAT_ABBREV_WEEKDAY;
+        view.setText(DateUtils.formatDateTime(this, millis, flags));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+}
