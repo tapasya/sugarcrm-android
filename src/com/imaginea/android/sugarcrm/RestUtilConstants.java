fa799654abde9a7296113802c1c6b46454d376de
chanderp <chanderp>
2010-07-22 08:01:07 +0000

    added CASE_ID and formatted

--- src/com/imaginea/android/sugarcrm/RestUtilConstants.java
+++ src/com/imaginea/android/sugarcrm/RestUtilConstants.java
@@ -14,7 +14,7 @@ public interface RestUtilConstants {
     public static final String APPLICATION = "application";
 
     public static final String NAME_VALUE_LIST = "name_value_list";
-    
+
     public static final String NAME_VALUE_LISTS = "name_value_lists";
 
     // input and response types for the request and response
@@ -32,23 +32,23 @@ public interface RestUtilConstants {
     public static final String GET_ENTRY = "get_entry";
 
     public static final String GET_MODULE_FIELDS = "get_module_fields";
-    
+
     public static final String SET_ENTRY = "set_entry";
-    
+
     public static final String SET_ENTRIES = "set_entries";
-    
+
     public static final String GET_ENTRIES_COUNT = "get_entries_count";
-    
+
     public static final String GET_RELATIONSHIPS = "get_relationships";
-    
+
     public static final String SET_RELATIONSHIP = "set_relationship";
-    
+
     public static final String SET_RELATIONSHIPS = "set_relationships";
-    
+
     public static final String SEARCH_BY_MODULE = "search_by_module";
-    
+
     public static final String GET_SERVER_INFO = "get_server_info";
-    
+
     public static final String GET_USER_ID = "get_user_id";
 
     // module names
@@ -62,6 +62,10 @@ public interface RestUtilConstants {
 
     public static final String MEETINGS_MODULE = "Meetings";
 
+    public static final String CALLS_MODULE = "Calls";
+
+    public static final String CASES_MODULE = "Cases";
+
     // params for the input JSON
     public static final String USER_AUTH = "user_auth";
 
@@ -72,13 +76,13 @@ public interface RestUtilConstants {
     public static final String SESSION = "session";
 
     public static final String PARENT_MODULE_NAME = "parent_module_name";
-    
+
     public static final String MODULE_NAME = "module_name";
-    
+
     public static final String MODULE_NAMES = "module_names";
-    
+
     public static final String BEAN_ID = "module_id";
-    
+
     public static final String BEAN_IDS = "module_ids";
 
     public static final String QUERY = "query";
@@ -88,29 +92,28 @@ public interface RestUtilConstants {
     public static final String OFFSET = "offset";
 
     public static final String SELECT_FIELDS = "select_fields";
-    
+
     public static final String LINK_NAME_TO_FIELDS_ARRAY = "link_name_to_fields_array";
-    
+
     public static final String LINK_FIELD_NAME = "link_field_name";
-    
+
     public static final String LINK_FIELD_NAMES = "link_field_names";
-    
+
     public static final String RELATED_MODULE_QUERY = "related_module_query";
-    
+
     public static final String RELATED_IDS = "related_ids";
-    
+
     public static final String RELATED_FIELDS = "related_fields";
-    
+
     public static final String RELATED_MODULE_LINK_NAME_TO_FIELDS_ARRAY = "related_module_link_name_to_fields_array";
-    
+
     public static final String MAX_RESULTS = "max_results";
-    
+
     public static final String DELETED = "deleted";
 
     public static final String FIELDS = "fields";
-    
+
     public static final String SEARCH_STRING = "search_string";
-    
 
     // params for the output JSON
     public static final String ID = "id";
@@ -134,23 +137,23 @@ public interface RestUtilConstants {
     public static final String LABEL = "label";
 
     public static final String REQUIRED = "required";
-    
+
     public static final String CREATED = "created";
-    
+
     public static final String FAILED = "failed";
 
     public static final String DESCRIPTION = "description";
 
     public static final String LOGIN_FAILED = "Login Failed!";
-    
+
     public static final String RECORDS = "records";
-    
+
     public static final String VALUE = "value";
-    
+
     public static final String MODULE = "module";
-    
+
     public static final String BEAN_NAME = "bean_name";
-    
+
     public static final String RELATIONSHIP = "relationship";
 
     // Exceptions
