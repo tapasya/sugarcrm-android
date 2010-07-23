45fcb356e24a00220700cba3ba3d49102d7a46f7
chanderp <chanderp>
2010-07-23 05:39:03 +0000

    notify utility

--- src/com/imaginea/android/sugarcrm/util/Util.java
+++ src/com/imaginea/android/sugarcrm/util/Util.java
@@ -1,6 +1,12 @@
 package com.imaginea.android.sugarcrm.util;
 
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.util.Log;
@@ -160,4 +166,57 @@ public class Util {
         return networkOn;
     }
 
+    /**
+     * Post notification, the package name of the context should be same as that of any activity you
+     * want to start.
+     * 
+     * @param context
+     *            - context of the component posting notification, can be an activity/service etc
+     * @param clazz
+     *            class of the optional activity that can be started
+     * @param tickerText
+     * @param title
+     * @param message
+     * 
+     * 
+     * @return ID of notification so it can be cancelled/updated
+     */
+    public static synchronized int notify(Context context, Class<Activity> clazz,
+                                    int tickerTextRes, int titleRes, String message) {
+
+        // ComponentName comp = new ComponentName(context, clazz);
+        return notify(context, context.getPackageName(), clazz, tickerTextRes, titleRes, message);
+
+    }
+
+    /**
+     * Post notification, the package name should be same as that of any activity clazz you want to
+     * start and the context passed can be in any other package
+     * 
+     * @param context
+     *            - context of the component posting notification, can be an activity/service etc
+     * @param clazz
+     *            class of the optional activity that can be started
+     * @param tickerText
+     * @param title
+     * @param message
+     * 
+     * 
+     * @return ID of notification so it can be cancelled/updated
+     */
+    public static synchronized int notify(Context context, String packageName, Class clazz,
+                                    int tickerTextRes, int titleRes, String message) {
+        CharSequence tickerText = context.getResources().getText(tickerTextRes);
+        CharSequence title = context.getResources().getText(titleRes);
+
+        NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        ComponentName comp = new ComponentName(packageName, clazz.getSimpleName());
+        Intent intent = new Intent().setComponent(comp);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, Intent.FLAG_ACTIVITY_NEW_TASK);
+        Notification n = new Notification(android.R.drawable.stat_notify_sync_noanim, tickerText, System.currentTimeMillis());
+        n.setLatestEventInfo(context, title, message, pendingIntent);
+        int id = getId();
+        nm.notify(id, n);
+        return id;
+    }
 }
