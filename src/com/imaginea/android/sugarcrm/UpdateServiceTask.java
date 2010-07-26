41b15263d4612b8ec4288fb461fabfe4f6634e22
chanderp <chanderp>
2010-07-26 09:06:36 +0000

    converted to switch statement and we do all cases here,  fixed the NPE when orphan module is inserted as beanId is not present

--- src/com/imaginea/android/sugarcrm/UpdateServiceTask.java
+++ src/com/imaginea/android/sugarcrm/UpdateServiceTask.java
@@ -20,6 +20,7 @@ import java.util.Map;
  * UpdateServiceTask
  * 
  * @author chander
+ * @author vasavi
  * 
  */
 public class UpdateServiceTask extends AsyncServiceTask<Object, Void, Object> {
@@ -91,29 +92,53 @@ public class UpdateServiceTask extends AsyncServiceTask<Object, Void, Object> {
             int updatedRows = 0;
             boolean serverUpdated = false;
             if (Util.isNetworkOn(mContext)) {
-                if (Log.isLoggable(LOG_TAG, Log.INFO)) {
+                if (Log.isLoggable(LOG_TAG, Log.DEBUG)) {
                     Log.i(LOG_TAG, "linkFieldName : " + mLinkFieldName);
                 }
-                if (mLinkFieldName != null) {
-                    updatedBeanId = RestUtil.setEntry(url, sessionId, mModuleName, mUpdateNameValueMap);
-                    RelationshipStatus status = RestUtil.setRelationship(url, sessionId, mParentModuleName, mBeanId, mLinkFieldName, new String[] { updatedBeanId }, new LinkedHashMap<String, String>(), 0);
-                    if (Log.isLoggable(LOG_TAG, Log.INFO)) {
-                        Log.i(LOG_TAG, "created: " + status.getCreatedCount() + " failed: "
-                                                        + status.getFailedCount() + " deleted: "
-                                                        + status.getDeletedCount());
-                    }
+                switch (mCommand) {
+                case R.id.insert:
+                    // inserts with a relationship
+                    if (mLinkFieldName != null) {
+                        updatedBeanId = RestUtil.setEntry(url, sessionId, mModuleName, mUpdateNameValueMap);
+                        RelationshipStatus status = RestUtil.setRelationship(url, sessionId, mParentModuleName, mBeanId, mLinkFieldName, new String[] { updatedBeanId }, new LinkedHashMap<String, String>(), 0);
+                        if (Log.isLoggable(LOG_TAG, Log.DEBUG)) {
+                            Log.i(LOG_TAG, "created: " + status.getCreatedCount() + " failed: "
+                                                            + status.getFailedCount()
+                                                            + " deleted: "
+                                                            + status.getDeletedCount());
+                        }
 
-                    if (status.getCreatedCount() >= 1) {
-                        if (Log.isLoggable(LOG_TAG, Log.INFO)) {
-                            Log.i(LOG_TAG, "Relationship is also set!");
+                        if (status.getCreatedCount() >= 1) {
+                            if (Log.isLoggable(LOG_TAG, Log.DEBUG)) {
+                                Log.i(LOG_TAG, "Relationship is also set!");
+                            }
+                        } else {
+                            if (Log.isLoggable(LOG_TAG, Log.DEBUG)) {
+                                Log.i(LOG_TAG, "setRelationship failed!");
+                            }
                         }
                     } else {
-                        if (Log.isLoggable(LOG_TAG, Log.INFO)) {
-                            Log.i(LOG_TAG, "setRelationship failed!");
+                        // insert case for an orphan module add without any relationship, the
+                        // updatedBeanId is actually a new beanId returned by server
+                        updatedBeanId = RestUtil.setEntry(url, sessionId, mModuleName, mUpdateNameValueMap);
+                        if (updatedBeanId != null) {
+                            if (Log.isLoggable(LOG_TAG, Log.VERBOSE)) {
+                                Log.v(LOG_TAG, "insert bean on server successful");
+                            }
+                            serverUpdated = true;
+                        } else {
+                            serverUpdated = false;
                         }
+                        // TODO:
+                        // If the update fails when the network is ON, display the message in the
+                        // activity
                     }
-                } else {
-                    // bean Id has to be put in the nameValuelist
+                    break;
+
+                // make the same calls for update and delete as delete only changes the DELETED flag
+                // to 1
+                case R.id.update:
+                case R.id.delete:
                     mUpdateNameValueMap.put(SugarCRMContent.SUGAR_BEAN_ID, mBeanId);
                     updatedBeanId = RestUtil.setEntry(url, sessionId, mModuleName, mUpdateNameValueMap);
                     if (mBeanId.equals(updatedBeanId)) {
@@ -124,45 +149,66 @@ public class UpdateServiceTask extends AsyncServiceTask<Object, Void, Object> {
                     } else {
                         serverUpdated = false;
                     }
-                    // TODO: If the update fails when the network is ON, display the message
+                    // TODO:
+                    // If the update fails when the network is ON, display the message in the
+                    // activity
+                    break;
                 }
+
             }
 
-            if (mCommand == R.id.insert) {
+            switch (mCommand) {
+            case R.id.insert:
                 for (String key : mUpdateNameValueMap.keySet()) {
                     values.put(key, mUpdateNameValueMap.get(key));
                 }
-                // add updatedBeanId to the values map
-                values.put(SugarCRMContent.SUGAR_BEAN_ID, updatedBeanId);
+                if (serverUpdated) {
+                    // add updatedBeanId to the values map
+                    values.put(SugarCRMContent.SUGAR_BEAN_ID, updatedBeanId);
+                } else {
+                    // we do not have a beanId to add to our valueMap
+                }
                 Uri insertResultUri = mContext.getContentResolver().insert(mUri, values);
                 Log.i(LOG_TAG, "insertResultURi - " + insertResultUri);
-            } else {
-                if (mCommand == R.id.update) {
+                break;
+
+            case R.id.update:
+                for (String key : mUpdateNameValueMap.keySet()) {
+                    values.put(key, mUpdateNameValueMap.get(key));
+                }
+                updatedRows = mContext.getContentResolver().update(mUri, values, null, null);
+                break;
+
+            case R.id.delete:
+                if (serverUpdated) {
+                    updatedRows = mContext.getContentResolver().delete(mUri, null, null);
+                } else {
+                    // this will update just the delete column, sets it to 1
                     for (String key : mUpdateNameValueMap.keySet()) {
                         values.put(key, mUpdateNameValueMap.get(key));
                     }
                     updatedRows = mContext.getContentResolver().update(mUri, values, null, null);
-                } else if (mCommand == R.id.delete) {
-                    updatedRows = mContext.getContentResolver().delete(mUri, null, null);
                 }
-                if (Log.isLoggable(LOG_TAG, Log.VERBOSE)) {
-                    // pass the success/failure msg to activity
-                    if (updatedRows > 0) {
-                        Log.v(LOG_TAG, "update successful");
-                    } else {
-                        Log.v(LOG_TAG, "update failed");
-                    }
+                break;
+
+            }
+
+            // pass the success/failure msg to activity
+            if (Log.isLoggable(LOG_TAG, Log.VERBOSE)) {
+                if (updatedRows > 0) {
+                    Log.v(LOG_TAG, "update successful");
+                } else {
+                    Log.v(LOG_TAG, "update failed");
                 }
             }
 
             if (!serverUpdated) {
                 // update the sync table
-            }
 
+            }
         } catch (Exception e) {
             Log.e(LOG_TAG, e.getMessage(), e);
         }
-
         return null;
     }
 
