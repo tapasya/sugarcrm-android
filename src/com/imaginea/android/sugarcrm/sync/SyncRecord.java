b9495ab5b2f0f9fe5d1d367a013a3aaf05b9218b
chanderp <chanderp>
2010-07-26 12:44:16 +0000

    initial version

---
+++ src/com/imaginea/android/sugarcrm/sync/SyncRecord.java
@@ -0,0 +1,13 @@
+package com.imaginea.android.sugarcrm.sync;
+
+public class SyncRecord {
+    public long _id;
+
+    public long syncId;
+
+    public int syncCommand;
+
+    public String moduleName;
+
+    public String relatedModuleName;
+}
