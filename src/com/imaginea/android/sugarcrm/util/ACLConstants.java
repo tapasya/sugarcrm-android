4d85d95201e0186d33239f74ffa11dde49ad7191
vasavin <vasavin>
2010-07-26 09:01:22 +0000

    initial version.

---
+++ src/com/imaginea/android/sugarcrm/util/ACLConstants.java
@@ -0,0 +1,25 @@
+package com.imaginea.android.sugarcrm.util;
+
+public interface ACLConstants {
+
+    public static final int ACL_ALLOW_ADMIN_DEV = 100;
+
+    public static final int ACL_ALLOW_ADMIN = 99;
+
+    public static final int ACL_ALLOW_ALL = 90;
+
+    public static final int ACL_ALLOW_ENABLED = 89;
+
+    public static final int ACL_ALLOW_OWNER = 75;
+
+    public static final int ACL_ALLOW_NORMAL = 1;
+
+    public static final int ACL_ALLOW_DEFAULT = 0;
+
+    public static final int ACL_ALLOW_DISABLED = -98;
+
+    public static final int ACL_ALLOW_NONE = -99;
+
+    public static final int ACL_ALLOW_DEV = 95;
+
+}
