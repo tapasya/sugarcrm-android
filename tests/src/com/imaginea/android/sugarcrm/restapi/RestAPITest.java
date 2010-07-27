cfa745f27c69004dfacd166f107e9521d62d3de4
chanderp <chanderp>
2010-07-27 09:53:07 +0000

    removed the extra server info test which slows down all tests, added account manager generic stuff - not complete yet, formatted

--- tests/src/com/imaginea/android/sugarcrm/restapi/RestAPITest.java
+++ tests/src/com/imaginea/android/sugarcrm/restapi/RestAPITest.java
@@ -1,70 +1,58 @@
 package com.imaginea.android.sugarcrm.restapi;
 
-import java.util.List;
+import android.accounts.AccountManager;
+import android.content.Context;
+import android.test.AndroidTestCase;
+import android.test.suitebuilder.annotation.SmallTest;
+
+import com.imaginea.android.sugarcrm.util.RestUtil;
 
 import junit.framework.TestCase;
 
 import org.json.JSONObject;
 
-import android.test.AndroidTestCase;
-import android.test.suitebuilder.annotation.SmallTest;
-import android.util.Log;
-
-import com.imaginea.android.sugarcrm.util.RestUtil;
-
 /**
  * 
  * Extend this class for module specific unit tests.
  * 
- * Since this test doesn't need a {@link android.content.Context}, or any other
- * dependencies injected, it simply extends the standard {@link TestCase}.
+ * Since this test doesn't need a {@link android.content.Context}, or any other dependencies
+ * injected, it simply extends the standard {@link TestCase}.
  * 
- * See {@link com.imaginea.android.sugarcrm.AllTests} for documentation on
- * running all tests and individual tests in this application.
+ * See {@link com.imaginea.android.sugarcrm.AllTests} for documentation on running all tests and
+ * individual tests in this application.
  */
 public class RestAPITest extends AndroidTestCase {
 
-	/**
-	 * used by module specific unit tests
-	 */
-	protected String url = "http://192.168.1.83/sugarcrm/service/v2/rest.php";
+    protected AccountManager mAccountManager;
 
-	protected String userName = "will";
+    protected Context mTargetContext;
 
-	protected String password = "will";
+    protected String mAccount;
+    /**
+     * used by module specific unit tests
+     */
+    protected String url = "http://192.168.1.83/sugarcrm/service/v2/rest.php";
 
-	protected String mSessionId;
+    protected String userName = "will";
 
-	@Override
-	protected void setUp() throws Exception {
+    protected String password = "will";
 
-		super.setUp();
-		authenticate();
-	}
+    protected String mSessionId;
 
-	public void authenticate() throws Exception {
+    @Override
+    protected void setUp() throws Exception {
 
-		mSessionId = RestUtil.loginToSugarCRM(url, userName, password);
-		assertNotNull(mSessionId);
-	}
+        super.setUp();
+       // mTargetContext = getInstrumentation().getTargetContext();
 
-	/*@SmallTest
-	public void testGetModules() throws Exception {
-		List<String> modules = RestUtil.getAvailableModules(url, mSessionId);
-		for (String module : modules) {
-			Log.d("RestAPITest", "\t" + module);
-		}
-	}*/
-
-	@SmallTest
-    public void testGetServerInfo() throws Exception{
-        String response = RestUtil.getServerInfo(url);
-        System.out.println("getServerInfo : " + response);
-        
-        JSONObject jsonResponse = new JSONObject(response);
-        assertNotNull(jsonResponse.get("flavor").toString());
-        assertNotNull(jsonResponse.get("version").toString());
-        assertNotNull(jsonResponse.get("gmt_time").toString());
+      //  mAccountManager = AccountManager.get(mTargetContext);
+       // mAccount = getAccount();
+        authenticate();
     }
 
+    public void authenticate() throws Exception {
+
+        mSessionId = RestUtil.loginToSugarCRM(url, userName, password);
+        assertNotNull(mSessionId);
+    }      
 }
