782e03d41ebc2b710957328426fe244c9ed8c59f
chanderp <chanderp>
2010-07-29 11:39:31 +0000

    updated the test to use DatabaseHelper

--- tests/src/com/imaginea/android/sugarcrm/restapi/ModuleFieldsOrderInsertTest.java
+++ tests/src/com/imaginea/android/sugarcrm/restapi/ModuleFieldsOrderInsertTest.java
@@ -1,16 +1,9 @@
 package com.imaginea.android.sugarcrm.restapi;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
-import com.imaginea.android.sugarcrm.provider.DatabaseHelper;
-
-import android.database.sqlite.SQLiteDatabase;
 import android.test.AndroidTestCase;
 import android.test.suitebuilder.annotation.SmallTest;
-import android.util.Log;
+
+import com.imaginea.android.sugarcrm.provider.DatabaseHelper;
 
 public class ModuleFieldsOrderInsertTest extends AndroidTestCase {
 
@@ -25,26 +18,9 @@ public class ModuleFieldsOrderInsertTest extends AndroidTestCase {
      */
     @SmallTest
     public void testSortOrderInsertion() throws Exception {
-        InputStream is = getContext().getAssets().open(SQL_FILE);
 
         DatabaseHelper dbHelper = new DatabaseHelper(getContext());
-        SQLiteDatabase db = dbHelper.getWritableDatabase();
-        db.beginTransaction();
+        dbHelper.executeSQLFromFile(SQL_FILE);
 
-        /*
-         * Use the openFileInput() method the ActivityContext provides. Again for security reasons
-         * with openFileInput(...)
-         */
-        BufferedReader br = new BufferedReader(new InputStreamReader(is));
-        String sql;
-        while ((sql = br.readLine()) != null) {
-            db.execSQL(sql);
-            if (Log.isLoggable(TAG, Log.DEBUG))
-                Log.d(TAG, "read from file: " + sql);
-        }
-
-        db.setTransactionSuccessful();
-        db.endTransaction();
-        db.close();
     }
 }
