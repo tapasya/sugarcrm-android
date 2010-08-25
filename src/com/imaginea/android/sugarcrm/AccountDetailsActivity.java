811bce745d893e95b9ed56715d87600e84c173ed
vasavin <vasavin>
2010-08-25 11:30:38 +0000

    modified to disable the 'related' item in the menu when the module is not related to any other modules.

--- src/com/imaginea/android/sugarcrm/AccountDetailsActivity.java
+++ src/com/imaginea/android/sugarcrm/AccountDetailsActivity.java
@@ -153,10 +153,14 @@ public class AccountDetailsActivity extends Activity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.details_activity_menu, menu);
 
-        SubMenu relationshipMenu = menu.addSubMenu(0, R.string.related, 0, getString(R.string.related));
+        SubMenu relationshipMenu = menu.addSubMenu(1, R.string.related, 0, getString(R.string.related));
         relationshipMenu.setIcon(R.drawable.menu_related);
-        for (int i = 0; i < mRelationshipModules.length; i++) {
-            relationshipMenu.add(0, Menu.FIRST + i, 0, mRelationshipModules[i]);
+        if (mRelationshipModules.length > 0) {
+            for (int i = 0; i < mRelationshipModules.length; i++) {
+                relationshipMenu.add(0, Menu.FIRST + i, 0, mRelationshipModules[i]);
+            }
+        } else{
+            menu.setGroupEnabled(1, false);
         }
 
         return true;
